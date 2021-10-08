837fc7969b276d881b0b12249c284459b5270150
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-08 11:25:12 +0900

    ファイルを追加 他のファイルの変更は忘れた

--- proc_if.js
+++ proc_if.js
@@ -1,19 +1,22 @@
 let result;
 let elem;
-let graph;
+// let graph;
+
 
 function init() {
     elem = document.getElementById("output");
     graph = document.getElementById("Graph1");
+    
     console.log(graph);
     result = [0];
-    
-
 }
 
 function exec() {
     getData(result);
     elem.innerHTML = "結果" + result;
+    graph.width = 300;
+    graph.script="setRange(-10, 10, -5, 5)";
+    console.log(graph);
 }
 
 function testFunc() {
