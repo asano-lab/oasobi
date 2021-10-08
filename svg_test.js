d462bcf84ffbdf4ab1b0eb1f46833439dfd77d3a
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-08 19:30:15 +0900

    末端が切れているのは目を瞑ろう 最初も(0, 0)スタートじゃなくていいかな

--- svg_test.js
+++ svg_test.js
@@ -7,14 +7,14 @@ function hogeFunc(x, y) {
 }
 
 function main() {
-    setRange(-6, 6, -6, 6);
+    setRange(-6, 10, -6, 6);
     axis('full',2,2,0,0);
     style.stroke = 'blue';
     plot('3cos(x)');
     Macro.myCommand(3, 4);
     style.stroke = 'red';
     style.strokeWidth = 2;
-    let tmp_array = [0, 1, 0, 1, 0, 1];
+    let tmp_array = [1, 1, 0, 0, 0, 1];
     let vertices = code2vertices(tmp_array);
     // console.log(1, 2);
     line(0, 0, 2, 2);
@@ -31,9 +31,6 @@ function code2vertices(code) {
     for (let i = 1; i < code.length; i++) {
         vertices.push([i, code[i - 1], i, code[i]]);
         vertices.push([i, code[i], i + 1, code[i]]);
-    }
-    for (let i = 0; i < vertices.length; i++) {
-        console.log(vertices[i]);
     }
     return vertices;
 }
