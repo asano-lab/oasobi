bbb297ff50bc589ac1e213d4fbe5163f740a3067
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-08 18:56:40 +0900

    Macro.main に全て処理を記述すれば良さそう??

--- svg_test.js
+++ svg_test.js
@@ -1,10 +1,20 @@
-
+// 適当な関数
 function hogeFunc(x, y) {
     let vx = fix(x, 0);
     let vy = fix(y, 0);
     dot(m.round(vx,2), m.round(vy,2), "dot", "("+x+","+y+")");
 }
 
+function main() {
+    console.log(setRange);
+    setRange(-6, 6, -6, 6);
+    axis('full',2,2,0,0);
+    style.stroke = 'blue';
+    plot('3cos(x)');
+    Macro.myCommand(3, 4);
+}
+
 (function() {
 	SVGGraph.registerMacro("myCommand", hogeFunc);
+    SVGGraph.registerMacro("main", main);
 })();
