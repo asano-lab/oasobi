775df83203d81624ec75105b7dc978659500da0f
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-22 00:23:56 +0900

    配列に複数の解答と登録し, includesメソッドで判定すれば対応できそう

--- hash.js
+++ hash.js
@@ -2,18 +2,25 @@
 
 {
     const TEST = document.getElementById("test");
+    const HASH_LIST = [
+        // 「ハッシュ関数」のハッシュ値
+        "105c60d27a2bc2266c55b6a22e5c1bdbfbc1ef084dac2d1b07eccd3be76e47f8", 
+        // 「はっしゅかんすう」のハッシュ値
+        "b101caf7405d2b771a5fbc7873382c349c661eb6c3cd331e1f80f5abfaa45c28"
+    ]
     let FIELD;
 
     window.onload = () => {
+        const _sha_obj = new jsSHA("SHA-256","TEXT");
         FIELD = document.getElementById("field");
         FIELD.addEventListener("input", hashCheck);
 
-        // let _text = "ハッシュ関数";
-        // SHA_OBJ.update(_text);
-        // TEST.textContent = SHA_OBJ.getHash("HEX");
+        let _text = "はっしゅかんすう";
+        _sha_obj.update(_text);
+        TEST.textContent = _sha_obj.getHash("HEX");
 
         // ハッシュ値を予め計算して記録
-        TEST.textContent = "105c60d27a2bc2266c55b6a22e5c1bdbfbc1ef084dac2d1b07eccd3be76e47f8";
+        // TEST.textContent = "105c60d27a2bc2266c55b6a22e5c1bdbfbc1ef084dac2d1b07eccd3be76e47f8";
     }
     
     let hashCheck = () => {
@@ -22,7 +29,7 @@
         const _hash = _sha_obj.getHash("HEX");
 
         // console.log(TEST.textContent);
-        if (TEST.textContent == _hash) {
+        if (HASH_LIST.includes(_hash)) {
             console.log("正解");
         } else {
             console.log("不正解");
