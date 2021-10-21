f36e7ebd77f80f635d68ecd3a98d51b109eb55c3
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-22 00:06:43 +0900

    入力イベントに応答してログを出力

--- hash.js
+++ hash.js
@@ -3,8 +3,16 @@
 {
     const SHA_OBJ = new jsSHA("SHA-256","TEXT");
     const TEST = document.getElementById("test");
+    let FIELD;
+
+    let hashCheck = () => {
+        console.log("あ");
+    }
 
     window.onload = () => {
+        FIELD = document.getElementById("field");
+        FIELD.addEventListener("input", hashCheck);
+
         let _text = "ハッシュ関数";
 
         SHA_OBJ.update(_text);
