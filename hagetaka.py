5bc184ac125e4d2671e550cb0596126225af9b26
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-22 13:11:23 +0900

    プレイヤークラスも作成

--- hagetaka.py
+++ hagetaka.py
@@ -4,12 +4,17 @@ class Game:
 
     def __init__(self):
         self.hagetaka = [True] * 15
-        self.hagetaka[5] = False
         print(self.hagetaka)
-        print("hello world")
+
+class Player:
+
+    def __init__(self):
+        self.cards = [True] * 15
+        print(self.cards)
 
 def main():
     g = Game()
+    p1 = Player()
 
 if __name__ == "__main__":
     main()
