bf77d7784bae55d672f2921c8f796a874ea7aa25
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-22 13:21:04 +0900

    プレイヤーが持っているカードを表示

--- hagetaka.py
+++ hagetaka.py
@@ -5,21 +5,24 @@ class Game:
     def __init__(self, p_list):
         self.hagetaka = [True] * 15
         print(self.hagetaka)
-        print(*p_list)
+        for i in p_list:
+            print(i)
 
 class Player:
 
     def __init__(self, name):
         self.name = name
         self.cards = [True] * 15
-        print(self.cards)
+        # print(self.cards)
     
     def __str__(self):
-        moji = self.name
+        moji = self.name + "\ncards: "
         for i, j in enumerate(self.cards):
-            if i:
-                moji += ", " + str(i)
-        moji += "\n"
+            if j:
+                moji += "{:2d}".format(i + 1)
+            if i != 14:
+                moji += ", "
+        # moji += "\n"
         return moji
 
 def main():
