62aafc462f3e0e6da6ad9fdbf9b7e4a7892517a2
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-24 13:26:45 +0900

    そもそもIPが正しいのかすら怪しい

--- samplegraph.py
+++ samplegraph.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 print(matplotlib.__version__)
-matplotlib.use("TkAgg")
+# matplotlib.use("TkAgg")
 
 x = np.linspace(0, 10, 100)
 y = np.random.randn(100)
