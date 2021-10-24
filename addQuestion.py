54fd26d76e32fd2cdac4aab5387f7a231c92f078
keiichiro-sonoda <mountain13577749@gmail.com>
2021-10-24 22:46:21 +0900

    抜けていたコロンを追加

--- addQuestion.py
+++ addQuestion.py
@@ -206,7 +206,7 @@ class Example(QWidget):
     # ファイルの中身を作成
     def makeMdFile(self):
         # ヘッダの作成
-        moji = "---\nlayout: post\ntitle \"第{:d}回\"\ndate: ".format(self.q_id)
+        moji = "---\nlayout: post\ntitle: \"第{:d}回\"\ndate: ".format(self.q_id)
         moji += self.now.strftime('%Y-%m-%d %H:%M:%S +0900\n')
         moji += "categories: question\n---\n\n"
         moji += "![第{:d}回　写真](/kokodoko/{:s})\n\n".format(self.q_id, self.fname_img)
