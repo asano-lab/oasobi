ba30dedcf4da78e4696bf35872a5b4956c8c215a
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-25 20:39:15 +0900

    modeの取得に成功

--- cgi-bin/yeah.py
+++ cgi-bin/yeah.py
@@ -9,8 +9,16 @@ print("Content-Type: text/html\n")
 print("<html><body>\n")
 print("<h3>Python CGI test</h3>")
 
-for var in form.keys():
-    val = form[var].value
-    print("%s = %s<br>" % (var, val))
+# for var in form.keys():
+#     val = form[var].value
+#     print("%s = %s<br>" % (var, val))
+
+# mode のみ取得
+if "mode" not in form:
+    mode = "notfound"
+else:
+    mode = str(form["mode"].value)
+
+print("mode=%s" % mode)
 
 print("</body></html>\n")
