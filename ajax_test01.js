ee1f59f72da5a900c4a7b602435376ed367c2be5
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-25 22:43:56 +0900

    全部アローにしても問題なし

--- ajax_test01.js
+++ ajax_test01.js
@@ -3,15 +3,14 @@ let xhttp;
 const loadDoc = (url, callBack) => {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-        console.log(this);
-        console.log(xhttp);
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             callBack(xhttp);
         }
     };
-    xhttp.open("GET",url,true);
+    xhttp.open("GET", url, true);
     xhttp.send();
 }
-function load(xhttp) {
+
+const load = (xhttp) => {
     document.getElementById("change").innerHTML = xhttp.responseText;
 }
