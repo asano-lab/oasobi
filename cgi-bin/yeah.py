66cb02a2696711b8e5f9a3f7c07087bda8b172e5
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-10-25 20:36:48 +0900

    組込のプログラム動かせたっぽい

---
+++ cgi-bin/yeah.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import cgi, cgitb
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+print("Content-Type: text/html\n")
+print("<html><body>\n")
+print("<h3>Python CGI test</h3>")
+
+for var in form.keys():
+    val = form[var].value
+    print("%s = %s<br>" % (var, val))
+
+print("</body></html>\n")
