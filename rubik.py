7fa51915c74e7ced6944e5c35a454677667de455
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-02 23:31:30 +0900

    多分数値変換できた

--- rubik.py
+++ rubik.py
@@ -17,8 +17,25 @@ class Rubik():
         ]
     ]
 
+    # 動かないマス
+    immovable = [9, 11, 13, 15, 24, 25, 26]
+
     def __init__(self) -> None:
         print(self.complete)
+        n = self.lll2num(self.complete)
+        print(bin(n))
+        print(len(bin(n)))
+    
+    # 3次元リストを数値に変換 (100bit)
+    def lll2num(self, lll: list) -> int:
+        n = 1
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    p = lll[i][j][k]
+                    if p not in self.immovable:
+                        n = (n << 5) | p
+        return n
     
     def bfs(self) -> None:
         pass
