37803c97a3c1b882e988a6667dcddbfc5fe4d8d8
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-03 01:06:56 +0900

    逆変換ミスってた

--- rubik.py
+++ rubik.py
@@ -22,9 +22,9 @@ class Rubik():
     immovable = {
         (0, 1, 1): 24,
         (1, 0, 1):  9,
-        (1, 1, 0): 11,
-        (1, 1, 1): 15,
-        (1, 1, 2): 25,
+        (1, 1, 0): 15,
+        (1, 1, 1): 25,
+        (1, 1, 2): 11,
         (1, 2, 1): 13,
         (2, 1, 1): 26
     }
@@ -35,11 +35,7 @@ class Rubik():
         print(bin(n))
         lll = self.num2lll(n)
         print(lll)
-        lcp = self.lllCopy(lll)
-        # print(lcp)
-        lcp[1][1][1] = 1000
-        print(lcp)
-        print(lll)
+        print(self.rightUp(lll))
     
     # 3次元リストを数値に変換 (100bit)
     def lll2num(self, lll: list) -> int:
@@ -64,6 +60,7 @@ class Rubik():
                         n >>= 5
         return lll
     
+    # 3次元リストのコピー
     def lllCopy(self, lll: list) -> list:
         lcp = [[] for i in range(3)]
         for i, j in enumerate(lll):
@@ -72,8 +69,17 @@ class Rubik():
         return lcp
                         
     # 右側を上方向に90deg回転
-    def rightUp(lll: list) -> list:
-        pass
+    def rightUp(self, cube: list) -> list:
+        n_cube = self.lllCopy(cube)
+        n_cube[0][0][2] = cube[0][2][2]
+        n_cube[0][2][2] = cube[2][2][2]
+        n_cube[2][2][2] = cube[2][0][2]
+        n_cube[2][0][2] = cube[0][0][2]
+        n_cube[0][1][2] = cube[1][2][2]
+        n_cube[1][2][2] = cube[2][1][2]
+        n_cube[2][1][2] = cube[1][0][2]
+        n_cube[1][0][2] = cube[0][1][2]
+        return n_cube
     
     def bfs(self) -> None:
         pass
