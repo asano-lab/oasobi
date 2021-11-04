e3f72e4e5835ca585fb75969802e9c98b28ea572
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-04 22:39:52 +0900

    ちょっとした表示ができた

--- animal_shogi.c
+++ animal_shogi.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "animal_shogi.h"
 
 int main(void) {
     printf("Hello World!!\n");
-    printf("%d\n", LION);
-    printf("%ld\n", INITIAL_BOARD);
+    showBoard(INITIAL_BOARD);
     return 0;
+}
+
+void showBoard(long b) {
+    u_char p;
+    for (int i = 44; i >= 0; i -= 4) {
+        p = (b >> i) & 0b1111;
+        putchar(DISPLAY_PEACES[p & 0b0111]);
+        if (p & 0b1000) {
+            putchar('2');
+        } else if (p) {
+            putchar('1');
+        } else {
+            putchar(' ');
+        }
+        putchar(' ');
+    }
+    putchar(10);
 }
--- animal_shogi.h
+++ animal_shogi.h
@@ -3,6 +3,11 @@
 
 #define INITIAL_BOARD (long)0xa003c914b002
 
+// 表示する文字
+#define DISPLAY_PEACES " HGELC"
+
 enum PEACES {EMPTY, CHICK, GIRAFFE, ELEPHANT, LION, CHICKEN};
+
+void showBoard(long b);
 
 #endif
