02aa8d6e5bc5b017d55a4590ee4cf607caa7f2a7
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-04 22:45:11 +0900

    多分盤面表示できた

--- animal_shogi.c
+++ animal_shogi.c
@@ -10,17 +10,20 @@ int main(void) {
 
 void showBoard(long b) {
     u_char p;
-    for (int i = 44; i >= 0; i -= 4) {
-        p = (b >> i) & 0b1111;
-        putchar(DISPLAY_PEACES[p & 0b0111]);
-        if (p & 0b1000) {
-            putchar('2');
-        } else if (p) {
-            putchar('1');
-        } else {
+    for (int i = 0; i < 4; i ++) {
+        for (int j = 0; j < 3; j++) {
+            p = (b >> (44 - i * 4 - j * 16)) & 0b1111;
+            putchar(DISPLAY_PEACES[p & 0b0111]);
+            if (p & 0b1000) {
+                putchar('2');
+            } else if (p) {
+                putchar('1');
+            } else {
+                putchar(' ');
+            }
             putchar(' ');
         }
-        putchar(' ');
+        putchar(10);
     }
     putchar(10);
 }
