d040be19176442e54fb726f3d9dc290b51cb0be1
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-04 23:00:29 +0900

    一応持ち駒表示部分も作成 想定通り動くかは知らない

--- animal_shogi.c
+++ animal_shogi.c
@@ -7,10 +7,22 @@ int main(void) {
     return 0;
 }
 
-void showBoard(long b) {
-    u_char p;
-    for (int i = 0; i < 4; i ++) {
-        for (int j = 0; j < 3; j++) {
+// 盤面表示
+void showBoard(u_long b) {
+    int i, j;
+    u_char p, own_p, own_p_num;
+    own_p = b >> 48;
+    printf("E: ");
+    for (i = 0; i < 3; i++) {
+        own_p_num = own_p >> (i * 2);
+        for (j = 0; j < own_p_num; j++) {
+            putchar(DISPLAY_PEACES[i]);
+            printf("2 ");
+        }
+    }
+    putchar(10);
+    for (i = 0; i < 4; i ++) {
+        for (j = 0; j < 3; j++) {
             p = (b >> (44 - i * 4 - j * 16)) & 0b1111;
             putchar(DISPLAY_PEACES[p & 0b0111]);
             if (p & 0b1000) {
@@ -23,6 +35,15 @@ void showBoard(long b) {
             putchar(' ');
         }
         putchar(10);
+    }
+    own_p = b >> 54;
+    printf("D: ");
+    for (i = 0; i < 3; i++) {
+        own_p_num = own_p >> (i * 2);
+        for (j = 0; j < own_p_num; j++) {
+            putchar(DISPLAY_PEACES[i]);
+            printf("1 ");
+        }
     }
     putchar(10);
 }
