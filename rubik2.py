8e9b13bdf17ae22af792f333b4174158b56dbba1
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-12 23:45:33 +0900

    白の面だけ出力してみた

--- rubik2.py
+++ rubik2.py
@@ -11,10 +11,25 @@ COMPLETE = [
     [5, 5, 5, 5, 5, 5, 5, 5]
 ]
 
+JPN_COLOR = ["白", "赤", "黄", "橙", "緑", "青"]
+
 class Rubik:
 
     def __init__(self, cube=COMPLETE):
         self.cube = cube
+    
+    def __str__(self):
+        sub = 0
+        moji = ""
+        for i in range(3):
+            for j in range(3):
+                if i == 1 and j == 1:
+                    moji += "白"
+                else:
+                    moji += JPN_COLOR[self.cube[0][sub]]
+                    sub += 1
+            moji += "\n"
+        return moji
 
 if __name__ == "__main__":
     r = Rubik()
