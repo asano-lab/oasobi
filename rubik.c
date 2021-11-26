281c98abee08a582d0e5236cded7e587f97250bb
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-26 20:47:31 +0900

    0xと0bミスってた

--- rubik.c
+++ rubik.c
@@ -6,11 +6,11 @@
 #define R_STATE_C (u_long)0x0274c81abc
 #define R_STATE_E (u_long)0x02a464118e80a96
 
-#define getCp(c_info, n) ((c_info) >> (((37 - 5 * (n))) & 0x111))
-#define getCo(c_info, n) ((c_info) >> (((35 - 5 * (n))) & 0x11))
+#define getCp(c_info, n) (((c_info) >> (37 - 5 * (n))) & 0b111)
+#define getCo(c_info, n) (((c_info) >> ((35 - 5 * (n)))) & 0b11)
 
-#define getEp(e_info, n) ((e_info) >> ((56 - 5 * (n)) & 0x1111))
-#define getEo(e_info, n) ((e_info) >> ((55 - 5 * (n)) & 0x1))
+#define getEp(e_info, n) (((e_info) >> (56 - 5 * (n))) & 0b1111)
+#define getEo(e_info, n) (((e_info) >> (55 - 5 * (n))) & 0b1)
 
 typedef unsigned long long u_long;
 
@@ -32,6 +32,7 @@ cState applyMove(const cState s1, const cState s2) {
     ns.e_info = 0;
     for (i = 0; i < 8; i++) {
         j = getCp(s2.c_info, i);
+        printf("%d\n", j);
         ns.c_info = (ns.c_info << 3) | getCp(s1.c_info, j);
         nco = (getCo(s1.c_info, j) + getCo(s2.c_info, i)) % 3;
         ns.c_info = (ns.c_info << 2) | nco;
@@ -39,7 +40,7 @@ cState applyMove(const cState s1, const cState s2) {
     for (i = 0; i < 12; i++) {
         j = getEp(s2.e_info, i);
         ns.e_info = (ns.e_info << 4) | getEp(s1.e_info, j);
-        neo = (getEo(s1.e_info, j) ^ getEo(s2.c_info, i));
+        neo = (getEo(s1.e_info, j) ^ getEo(s2.e_info, i));
         ns.e_info = (ns.e_info << 1) | neo;
     }
     return ns;
