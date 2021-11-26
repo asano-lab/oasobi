bc6cdf146c02b8555e2250c308e0e7281e70e615
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-11-26 18:16:32 +0900

    完成状態の数値を定義した

--- rubik.c
+++ rubik.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define SOLVED_C 0x0110c8531c
+#define SOLVED_E 0x008864298e84a96
+
 typedef unsigned long long u_long;
 
 typedef struct {
@@ -14,5 +17,8 @@ int nibai(int x) {
 int main(void) {
     puts("Hello World!!");
     cState s;
-    printf("%I64d\n", sizeof s);
+    s.c_info = SOLVED_C;
+    s.e_info = SOLVED_E;
+    printf("0x%I64x\n", s.c_info);
+    printf("0x%I64x\n", s.e_info);
 }
