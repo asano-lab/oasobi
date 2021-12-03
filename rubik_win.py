d9ce979eeb0e24da9d5b0e304c9c398dc5d897b1
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-04 00:48:41 +0900

    桁をそろえた

--- rubik_win.py
+++ rubik_win.py
@@ -306,12 +306,12 @@ scrambled_state = scrambled_state.toState()
 print(scrambled_state)
 
 # Cで格納するための順番
-# cl_list = [
-#     "UL", "UR", "UB", "DF", "DL", "DR", "DB",
-#     "LU", "LD", "LF", "LB", "RU", "RD", "RF", "RB",
-#     "FU", "FD", "FL", "FR", "BU", "BD", "BL", "BR"
-# ]
+cl_list = [
+    "UL", "UR", "UB", "DF", "DL", "DR", "DB",
+    "LU", "LD", "LF", "LB", "RU", "RD", "RF", "RB",
+    "FU", "FD", "FL", "FR", "BU", "BD", "BL", "BR"
+]
 
-# for i in cl_list:
-#     print(i)
-#     print(change_color[i].toState2())
+for i in cl_list:
+    print(i)
+    print(change_color[i].toState2())
