6ff6faa8596777b77ee10094c886341f1b2c877b
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-03 23:29:33 +0900

    BUのスクランブルチェック

--- rubik_win.py
+++ rubik_win.py
@@ -245,6 +245,7 @@ change_color = {
 change_color["UB"] = change_color["UL"] * 2
 change_color["UR"] = change_color["UL"] * 3
 change_color["DB"] = change_color["FD"] * 2
+change_color["BU"] = change_color["FD"] * 3
 change_color["LD"] = change_color["FD"] + change_color["UL"]
 change_color["FL"] = change_color["UL"] + change_color["FD"]
 
@@ -288,7 +289,7 @@ for move_name in scramble:
 scrambled_state = scrambled_state.toState()
 print(scrambled_state)
 # print(scrambled_state.changeColor("UL"))
-print(scrambled_state.changeColor("DB"))
+print(scrambled_state.changeColor("BU"))
 
 # print(moves["L"])
 # print(moves["R"])
@@ -303,8 +304,8 @@ print(scrambled_state.changeColor("DB"))
 # print(moves["U'"])
 # print(moves["U'"].changeColor("FD"))
 # print(moves["R"].changeColor("UR"))
-print(moves["U"].changeColor("DB"))
-print(moves["R"].changeColor("DB"))
+print(moves["U"].changeColor("BU"))
+print(moves["R"].changeColor("BU"))
 
 # 多分右回しと等価
 # cl = ["LF", "LF", "LF", "BU", "LF"]
