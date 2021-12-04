fa0b1a8f5b0b5569ae9b88e16ebe2bcd09ef3ef4
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-04 15:26:13 +0900

    applyMoveを使いまわせることに気づいた

--- rubik_win.c
+++ rubik_win.c
@@ -97,16 +97,17 @@ int changeColor(const u_long *src, u_long *dst, int ch_rule) {
     dst[0] = 0;
     dst[1] = 0;
     applyMove(src, CHANGE_COLOR + ch_rule * 2, tmpst);
-    for (i = 0; i < 40; i += 5) {
-        j = getCp5(tmpst[0], i) * 5;
-        dst[0] = dst[0] << 3 | getCp5(rp[0], j);
-        dst[0] = dst[0] << 2 | (getCo5(tmpst[0], i) + getCo5(rp[0], j)) % 3;
-    }
-    for (i = 0; i < 60; i += 5) {
-        j = getEp5(tmpst[1], i) * 5;
-        dst[1] = dst[1] << 4 | getEp5(rp[1], j);
-        dst[1] = dst[1] << 1 | (getEo5(tmpst[1], i) ^ getEo5(rp[1], j));
-    }
+    applyMove(rp, tmpst, dst);
+    // for (i = 0; i < 40; i += 5) {
+    //     j = getCp5(tmpst[0], i) * 5;
+    //     dst[0] = dst[0] << 3 | getCp5(rp[0], j);
+    //     dst[0] = dst[0] << 2 | (getCo5(tmpst[0], i) + getCo5(rp[0], j)) % 3;
+    // }
+    // for (i = 0; i < 60; i += 5) {
+    //     j = getEp5(tmpst[1], i) * 5;
+    //     dst[1] = dst[1] << 4 | getEp5(rp[1], j);
+    //     dst[1] = dst[1] << 1 | (getEo5(tmpst[1], i) ^ getEo5(rp[1], j));
+    // }
     return 0;
 }
 
