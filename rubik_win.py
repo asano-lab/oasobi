4fa99ff014b0d51532ce96fdcfe443674227fa9d
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-05 18:14:07 +0900

    どうぶつしょうぎの使いまわしでファイルを作れそう

--- rubik_win.py
+++ rubik_win.py
@@ -1,5 +1,8 @@
 from ctypes import CDLL, c_int32, c_ulonglong
 import time
+import os
+import pickle
+import json
 
 clib = CDLL("./rubik_win.so")
 
@@ -94,6 +97,8 @@ MIRROR_POS = {
         [3, 2, 1, 0, 6, 5, 4, 7, 10, 9, 8, 11]
     ]
 }
+
+ST_LEN_MAX = 5000000
 
 # 資料通りのクラス
 class State():
@@ -424,7 +429,7 @@ class Search:
         for _ in range(depth):
             self._calcNeighbors(self.solved_neighbors)
         self.solved_neighbors_depth = max(self.solved_neighbors)
-    
+
     def searchTargetInSolvedNeighbors(self):
         """
         完成状態の近所にターゲットが含まれているか確認
@@ -634,6 +639,119 @@ def inputState():
             st = colorArray2State(color_array)
     return st
 
+def createSolvedNeighborsFile():
+    """
+    まずは全状態を洗い出したい
+    """
+    dir_path = "./dat/"
+    searching = dir_path + "searching.json"
+    path_format = dir_path + "act{:03d}_{:03d}.pickle"
+    act_num = 0
+    sub_num = 0
+
+    # まだ何も作られていない
+    # 最初のファイルを作成し, 深さ1の探索も行う
+    if not os.path.exists(searching):
+        fnamew = path_format.format(0, 0)
+        with open(fnamew, "wb") as f:
+            # 多分無意味だが一応正規化
+            st_num = solved.toNumNormal()
+            # 要素が1つだけの集合をファイルに書き込む
+            pickle.dump(set([st_num]), f)
+        fnamer = fnamew
+    # ファイルが存在する
+    else:
+        # 探索済みファイルを読み出し
+        with open(searching, "r") as f:
+            act_num, sub_num = json.load(f)
+
+        # 副番号をインクリメントしてファイルの存在を確認
+        sub_num += 1
+        fnamer = path_format.format(act_num, sub_num)
+        # 存在しない (この深さの盤面はすべて探索済み)
+        if not os.path.exists(fnamer):
+            # 次の深さの最初のファイルを指定
+            act_num += 1
+            # 副番号はリセット
+            sub_num = 0
+            fnamer = path_format.format(act_num, sub_num)
+
+    print(fnamer, "から次の状態を計算")
+
+    # ロード
+    with open(fnamer, "rb") as f:
+        prev_st_nums = pickle.load(f)
+
+    print("探索状態数：{:d}".format(len(prev_st_nums)))
+
+    next_st_nums = []
+
+    # 次の状態を計算
+    while prev_st_nums:
+        st_num = prev_st_nums.pop()
+        next_st_nums += applyAllMovesNormal(st_num)
+
+    # 探索情報を更新
+    with open(searching, "w") as f:
+        json.dump([act_num, sub_num], f)
+
+    print("新状態数 (重複排除前)：{:d}".format(len(next_st_nums)))
+
+    # 集合に変換
+    next_st_nums = set(next_st_nums)
+    # なんとなく初期化
+    known_st_nums = set()
+    latest_act_num = 0
+    latest_sub_num = 0
+
+    # 探索済み状態との重複を削除
+    for i in range(act_num + 2):
+        j = 0
+        past_fname = path_format.format(i, j)
+        while os.path.exists(past_fname):
+            with open(past_fname, "rb") as f:
+                # ループ終了時, 最新ファイルの状態が格納される
+                known_st_nums = pickle.load(f)
+            next_st_nums -= known_st_nums
+            # 最新ファイル名更新
+            latest_fname = past_fname
+            latest_act_num = i
+            latest_sub_num = j
+            j += 1
+            past_fname = path_format.format(i, j)
+    
+    # リストに変換
+    next_st_nums = list(next_st_nums)
+    print("新状態数 (重複排除後)：{:d}".format(len(next_st_nums)))
+
+    # 全探索終了 (ルービックキューブでは無理)
+    if not next_st_nums:
+        return True
+
+    # 深さ更新
+    if latest_act_num == act_num:
+        latest_act_num += 1
+        latest_sub_num = 0
+        latest_fname = path_format.format(latest_act_num, latest_sub_num)
+    # 更新しない場合, 最新ファイルの状態と足す
+    else:
+        next_st_nums = known_st_nums + next_st_nums
+
+    # 分割してファイルに保存
+    while len(next_st_nums) > ST_LEN_MAX:
+        with open(latest_fname, "wb") as f:
+            pickle.dump(set(next_st_nums[:ST_LEN_MAX]), f)
+        # 分割した残り
+        next_st_nums = next_st_nums[ST_LEN_MAX:]
+        # 最新番号の更新
+        latest_sub_num += 1
+        latest_fname = path_format.format(latest_act_num, latest_sub_num)
+    if next_st_nums:
+        with open(latest_fname, "wb") as f:
+            pickle.dump(set(next_st_nums), f)
+
+    return False
+
 # パーツの入れ替え辞書を作成
 replace_parts = {}
 for k, v in change_color.items():
@@ -666,15 +784,5 @@ for i, move_name in enumerate(scramble):
         break
     scrambled_state += moves[move_name]
 
-# scrambled_state = inputState()
-print(scrambled_state)
-print(solved.toNum())
-print(normalState(scrambled_state.toNum()))
-srch = Search(scrambled_state)
-srch.calcSolvedNeighbors(6)
-print(srch.searchTargetInSolvedNeighbors())
-print(srch.searchTargetBid(6))
-sacts = srch.getSolveMoves()
-for i in sacts:
-    print(i, end=" ")
-print()
+for i in range(3):
+    createSolvedNeighborsFile()
