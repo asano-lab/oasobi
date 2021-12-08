d53b726b28efe7be6daf7a882bb4a58d6f48f6eb
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-08 21:53:48 +0900

    なんとなくsubsetに変更 かえってわかりづらいかな

--- rubik_files_win.py
+++ rubik_files_win.py
@@ -1,12 +1,15 @@
 import pickle
 import rubik_win
 
+SUBSET_PATH_FORMAT = rubik_win.SMP_DIR_PATH + "subset_act{:03d}.pickle"
+
 def sampleAct9():
     """
     9手状態のデータ数が多すぎるのでランダム抽出したい
     """
     print(rubik_win.SN_PATH_FORMAT.format(9, 1))
-    print(rubik_win.SMP_DIR_PATH)
+    # print(rubik_win.SMP_DIR_PATH)
+    print(SUBSET_PATH_FORMAT.format(1))
     pass
 
 if __name__ == "__main__":
