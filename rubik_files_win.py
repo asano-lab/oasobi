1f2c6f000a9abd5b116281ef61780c6180e43029
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-08 21:49:32 +0900

    自作ファイルをimportできた

---
+++ rubik_files_win.py
@@ -0,0 +1,12 @@
+import pickle
+import rubik_win
+
+def sampleAct9():
+    """
+    9手状態のデータ数が多すぎるのでランダム抽出したい
+    """
+    print(rubik_win.SN_PATH_FORMAT.format(9, 1))
+    pass
+
+if __name__ == "__main__":
+    sampleAct9()
