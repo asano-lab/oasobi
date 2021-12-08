7abe5e5a35e9dbd0868390b39f5b952733caef84
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-08 22:09:51 +0900

    とりあえず36万のサンプルを取り出した

--- rubik_files_win.py
+++ rubik_files_win.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import os
 import random
+import time
 import rubik_win
 
 SUBSET_PATH_FORMAT = rubik_win.SMP_DIR_PATH + "subset_act{:03d}.pickle"
@@ -24,21 +25,23 @@ def sampleAct9():
     9手状態のデータ数が多すぎるのでランダム抽出したい.
     3億6千万くらいあるので, 1000分の1取り出したい.
     """
+    t0 = time.time()
+    st_subset = []
     for i in range(rubik_win.LOOP_MAX):
         fnamer = rubik_win.SN_PATH_FORMAT.format(9, i)
         sts = readPickleFile(fnamer)
         if sts is None:
             break
+        print(fnamer + "からロード")
         smp_num = len(sts) // 1000
         print("使うデータ数：%d" % smp_num)
-    print(rubik_win.SN_PATH_FORMAT.format(9, 1))
-    # print(rubik_win.SMP_DIR_PATH)
-    print(SUBSET_PATH_FORMAT.format(1))
-    pass
+        st_subset += random.sample(list(sts), smp_num)
+    fnamew = SUBSET_PATH_FORMAT.format(9)
+    print("部分集合のサイズ：%d" % len(st_subset))
+    # 集合にして保存
+    rubik_win.writeAndBackup(fnamew, set(st_subset))
+    print("%02d:%02d:%02d" % rubik_win.s2hms(time.time() - t0))
 
 if __name__ == "__main__":
-    a = {1, 2, 3, 4, 5}
-    print(random.sample(list(a), 5))
-    print(a)
-    # sampleAct9()
+    sampleAct9()
     pass
