6a1a4b0c38cb413b6005ce98303db98c227f1fac
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-12 15:08:49 +0900

    圧縮してもロードは大して時間かからない

--- rubik_files_win.py
+++ rubik_files_win.py
@@ -166,13 +166,13 @@ def createNpz():
         print("%02d:%02d:%02d" % rubik_win.s2hms(time.time() - t1))
 
 if __name__ == "__main__":
-    createNpz()
+    # createNpz()
     # checkSetSize()
-    # for i in range(7):
-    #     fnamer = TT_NPZ_PATH_FORMAT.format(i)
-    #     arrs = np.load(fnamer)
-    #     print(arrs.files)
-    #     print(arrs["train"].shape, arrs["test"].shape)
+    for i in range(11):
+        fnamer = TT_NPZ_PATH_FORMAT.format(i)
+        arrs = np.load(fnamer)
+        print(arrs.files)
+        print(arrs["train"].shape, arrs["test"].shape)
     # sampleActLT10(7, 0.2)
     # createSampleNpFile(7)
     pass
