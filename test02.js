2e24103b65df629e40d405017229e0c509a2d047
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-23 19:13:22 +0900

    いろいろ変更

--- test02.js
+++ test02.js
@@ -10,13 +10,23 @@ let prev_value = text.value;
 
 let comp = false;
 
+let start, end;
+
 text.addEventListener("textInput", (e) => {
     console.log(e);
+    console.log(start, end);
+    text.selectionStart = start;
+    text.selectionEnd = end;
     e.preventDefault();
 });
 
 text.addEventListener("input", (e) => {
     text.value = prev_value;
+    // console.log(text.selectionStart);
+    // console.log(text.selectionEnd);
+    text.selectionStart = start;
+    text.selectionEnd = end;
+    e.preventDefault();
     // console.log(e);
     // console.log(prev_value);
     // if (e.data !== null) {
@@ -81,9 +91,35 @@ text.addEventListener("keydown", (e) => {
                 }
             } else {
                 text.value = mae + ato;
+                text.selectionStart = start;
+                text.selectionEnd = start;
             }
         }
+        else if (e.code == "Delete") {
+            if (start == end) {
+                text.value = mae + ato.slice(1, ato.length);
+            } else {
+                text.value = mae + ato;
+            }
+            text.selectionStart = start;
+            text.selectionEnd = start;
+        }
         prev_value = text.value;
-        e.preventDefault();
+    } else if (e.code == "ArrowLeft") {
+        if (start > 0) {
+            text.selectionStart = start - 1;
+            text.selectionEnd = start - 1;
+        }
+    } else if (e.code == "ArrowRight") {
+        if (start < prev_value.length) {
+            text.selectionStart = start + 1;
+            text.selectionEnd = start + 1;
+        }
     }
+    e.preventDefault();
+});
+
+text.addEventListener("compositionend", (e) => {
+    console.log("yeah");
+    console.log(e);
 });
