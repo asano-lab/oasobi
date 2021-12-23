85cd923740e96c76714d4ed75f855b2115f3746f
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-23 11:32:14 +0900

    もしかしたらcomp変数で対応できたかもしれない

--- test02.js
+++ test02.js
@@ -7,6 +7,8 @@ let DELETE_TYPES = ["deleteContentBackward", "deleteContentForward"];
 
 let prev_value = text.value;
 
+let comp = false;
+
 // text.addEventListener("textInput", (e) => {
 //     console.log(e);
 // });
@@ -17,29 +19,26 @@ text.addEventListener("input", (e) => {
     if (e.data !== null) {
         console.log(e.data.length);
     }
+    console.log(comp);
+
     if (e.isComposing) {
         console.log(e.data);
         text.value = prev_value;
-        e.preventDefault();
+        comp = true;
     }
     else if (VALID_VALUES.includes(e.data)) {
         prev_value = text.value;
         console.log("有効な入力");
+        comp = false;
     }
     else if (DELETE_TYPES.includes(e.inputType)) {
-        console.log("削除");
-        text.value = prev_value.slice(0, -1);
-        prev_value = text.value;
+        if (!comp) {
+            console.log("削除");
+            prev_value = text.value;
+        }
     }
     else {
         text.value = prev_value;
+        comp = false;
     }
-});
-
-text.addEventListener("compositionstart", (e) => {
-    console.log("全角入力開始");
-});
-
-text.addEventListener("compositionend", (e) => {
-    console.log("全角入力終了");
 });
