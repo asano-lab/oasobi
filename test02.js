24712657346bd8c995b6a75f2fe05e9fa1ef7ada
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-23 10:53:14 +0900

    不完全な入力制限

--- test02.js
+++ test02.js
@@ -1,2 +1,21 @@
 const text = document.getElementById("text1");
-console.log(text);
+// console.log(text);
+
+let VALID_VALUES = ["0", "1"];
+
+let DELETE_TYPES = ["deleteContentBackward"];
+
+let prev_value = "";
+
+text.addEventListener("input", (e) => {
+    console.log(e);
+    if (VALID_VALUES.includes(e.data)) {
+        prev_value = text.value;
+    }
+    else if (DELETE_TYPES.includes(e.inputType)) {
+        prev_value = text.value;
+    }
+    else {
+        text.value = prev_value;
+    }
+});
