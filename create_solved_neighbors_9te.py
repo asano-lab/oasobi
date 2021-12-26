126ac69dee1aef98b6b1fc304142e61764202030
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-26 12:49:45 +0900

    実行形式に変更

--- create_solved_neighbors_9te.py
+++ create_solved_neighbors_9te.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from rubik_module import solved
 
 if __name__ == "__main__":
--- rubik_module.py
+++ rubik_module.py
@@ -1,4 +1,3 @@
-# !/usr/bin/python3
 from ctypes import CDLL, c_int32, c_ulonglong
 import time
 import os
