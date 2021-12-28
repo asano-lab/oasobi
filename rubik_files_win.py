37a9f755da658e3a06231c93a368a7f3939057ca
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2021-12-28 11:51:54 +0900

    ファイル削除

--- rubik_files_win.py
+++ rubik_files_win.py
@@ -16,7 +16,7 @@ SUBSET_NP_PATH_FORMAT = rubik_win.NP_DIR_PATH + "subset_act{:03d}.npy"
 TT_NPZ_PATH_FORMAT = rubik_win.NP_DIR_PATH + "train_test_act{:03d}.npz"
 ONEHOT_TT_NPZ_PATH_FORMAT = rubik_win.NP_DIR_PATH + "onehot_train_test_act{:03d}.npz"
 MERGED_SMP_PATH = SMP_DIR_PATH + "merged_sample016.pickle"
-MERGED_SMP_PATH = SMP_DIR_PATH + "merged_sample016_test.pickle"
+# MERGED_SMP_PATH = SMP_DIR_PATH + "merged_sample016_test.pickle"
 
 def set2nparrayBin(num_set):
     """
@@ -284,7 +284,7 @@ if __name__ == "__main__":
         if m:
             print(fnamer)
             mergeSampleFiles16(fnamer)
-            # os.remove(SMP_DIR_PATH + fnamer)
+            os.remove(SMP_DIR_PATH + fnamer)
         else:
             # print(fnamer)
             pass
