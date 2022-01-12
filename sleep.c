328a4eb1884c880f7a9b4602287756f5155dd2e9
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-01-12 13:52:53 +0900

    ファイル書き込みできた 開いて閉じるを毎回しないとリアルタイムで見れないらしい

--- sleep.c
+++ sleep.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(void) {
+    FILE *fp;
+    char fname[FILENAME_MAX] = "sleep.txt";
     printf("Hello World!!\n");
+    if ((fp = fopen(fname, "a")) == NULL) {
+        printf("\a%s can't be opened!!\n", fname);
+        return -1;
+    }
     for (int i = 0; i < 10; i++) {
         usleep(100000);
-        printf("%d\n", i);
+        fprintf(fp, "%d\n", i);
     }
+    fclose(fp);
     return 0;
 }
