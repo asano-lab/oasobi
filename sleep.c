bea642e74e23634f1eaa0dce64a8b97c7c49f329
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-01-12 14:01:07 +0900

    ファイルの開閉を繰り返してリアルタイムで表示できた

--- sleep.c
+++ sleep.c
@@ -2,18 +2,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(void) {
+int writeFile(const char *fname, const char *str) {
     FILE *fp;
-    char fname[FILENAME_MAX] = "sleep.txt";
-    printf("Hello World!!\n");
     if ((fp = fopen(fname, "a")) == NULL) {
         printf("\a%s can't be opened!!\n", fname);
         return -1;
     }
-    for (int i = 0; i < 10; i++) {
-        usleep(100000);
-        fprintf(fp, "%d\n", i);
-    }
+    fprintf(fp, "%s", str);
     fclose(fp);
+    return 0;
+}
+
+int main(void) {
+    char fname[FILENAME_MAX] = "sleep.txt";
+    char str[BUFSIZ];
+    printf("Hello World!!\n");
+    for (int i = 0; i < 10; i++) {
+        usleep(500000);
+        snprintf(str, BUFSIZ, "%d\n", i);
+        writeFile(fname, str);
+    }
     return 0;
 }
