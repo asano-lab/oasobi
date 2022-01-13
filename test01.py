3c58f86e3104eb629bff55d878c7c53814a537f1
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-01-13 16:11:06 +0900

    一定確率で停止

--- test01.py
+++ test01.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import sys
 import time
+import random as rd
+
+r = 0
 
 try:
     for i in range(1000000):
+        time.sleep(1)
         print(i)
         print(sys.version)
-        time.sleep(1)
+        r = rd.randint(0, 19)
+        print(r)
+        if r == 0:
+            break
 
 except KeyboardInterrupt:
     pass
