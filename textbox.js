e9a985111e09865ae8bf46d17c8569517dedb476
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-01-13 11:50:28 +0900

    イベントチェック

--- textbox.js
+++ textbox.js
@@ -1,9 +1,120 @@
+const VALID_VALUES = ["0", "1"];
+const VALID_CODES = [
+    "Digit0", "Digit1", "Backspace", "Delete",
+    "ArrowLeft", "ArrowRight", "KeyA"
+];
 
 class MyTextBox {
     constructor(textbox) {
-        console.log("Hello!!");
+
         this.textbox = textbox;
+
+        // 変数初期化
+        this.prev_value = textbox.value;
+        this.start = this.textbox.selectionStart;
+        this.end = this.textbox.selectionEnd;
+        this.base = this.start;
+
+        // keydownイベントの処理を記述
+        this.textbox.addEventListener("keydown", (e) => {
+            // カーソル前後の文字列
+            let mae, ato;
+            console.log(e);
+
+            if (VALID_CODES.includes(e.code)) {
+                [this.start, this.end] = this.getSelections();
+                console.log(this.start, this.end);
+        
+                mae = this.prev_value.slice(0, this.start);
+                ato = this.prev_value.slice(this.end, this.prev_value.length);
+        
+                if (e.code == "Digit0") {
+                    this.textbox.value = mae + "0" + ato;
+                    this.setSelections(this.start + 1, this.start + 1);
+                } else if (e.code == "Digit1") {
+                    this.textbox.value = mae + "1" + ato;
+                    this.setSelections(this.start + 1, this.start + 1);
+                } else if (e.code == "Backspace") {
+                    if (this.start == this.end) {
+                        this.textbox.value = mae.slice(0, -1) + ato;
+                        if (start > 0) {
+                            this.setSelections(start - 1, start - 1);
+                        }
+                    } else {
+                        this.textbox.value = mae + ato;
+                        this.setSelections(this.start, this.start);
+                    }
+                } else if (e.code == "Delete") {
+                    if (this.start == this.end) {
+                        this.textbox.value = mae + ato.slice(1, ato.length);
+                    } else {
+                        this.textbox.value = mae + ato;
+                    }
+                    this.setSelections(this.start, this.start);
+                } else if (e.code == "ArrowLeft") {
+                    if (e.shiftKey) {
+                        if (this.start == this.end) {
+                            this.base = this.start;
+                            this.setSelections(this.start - 1, this.base);
+                        } else if (this.base < this.end) {
+                            this.setSelections(this.base, this.end - 1);
+                        } else if (this.start > 0) {
+                            this.setSelections(this.start - 1, this.base);
+                        }
+                    } else {
+                        if (this.start != this.end) {
+                            this.setSelections(this.start, this.start);
+                        } else if (this.start > 0) {
+                            this.setSelections(this.start - 1, this.start - 1);
+                        }
+                    }
+                } else if (e.code == "ArrowRight") {
+                    if (e.shiftKey) {
+                        if (this.start == this.end) {
+                            this.base = this.start;
+                            this.setSelections(this.base, this.end + 1);
+                        } else if (this.start < this.base) {
+                            this.setSelections(this.start + 1, this.base);
+                        }
+                        else if (this.start > 0) {
+                            this.setSelections(this.base, this.end + 1);
+                        }
+                    } else {
+                        if (this.start != this.end) {
+                            this.setSelections(this.end, this.end);
+                        } else if (this.start < this.prev_value.length) {
+                            this.setSelections(this.start + 1, this.start + 1);
+                        }
+                    }
+                } else if (e.code == "KeyA") {
+                    if (e.ctrlKey) {
+                        this.setSelections(0, this.prev_value.length);
+                    }
+                }
+                [this.start, this.end] = this.getSelections();
+                this.prev_value = this.textbox.value;
+            } else {
+                this.textbox.value = this.prev_value;
+            }
+            e.preventDefault();
+        });  
     }
+
+    // カーソル位置の取得
+    getSelections() {
+        return [this.textbox.selectionStart, this.textbox.selectionEnd]
+    }
+    
+    // カーソル位置の設定
+    setSelections(s1, s2) {
+        if (s1 < s2) {
+            this.textbox.selectionStart = s1;
+            this.textbox.selectionEnd = s2;
+        } else {
+            this.textbox.selectionStart = s2;
+            this.textbox.selectionEnd = s1;
+        }
+    };
 }
 
 const text = document.getElementById("text1");
@@ -11,175 +122,13 @@ const text = document.getElementById("text1");
 
 let password = document.getElementById("text2");
 
-const VALID_VALUES = ["0", "1"];
-const VALID_CODES = [
-    "Digit0", "Digit1", "Backspace", "Delete",
-    "ArrowLeft", "ArrowRight", "KeyA"
-];
-
 let DELETE_TYPES = ["deleteContentBackward", "deleteContentForward"];
 
 let prev_value = text.value;
 
 let comp = false;
 
-let start, end, base;
-
 mtb = new MyTextBox(document.getElementById("text1"));
-
-const resetTextSelection = () => {
-    text.selectionStart = start;
-    text.selectionEnd = end;
-};
-
-const setTextSelection = (s1, s2) => {
-    if (s1 < s2) {
-        text.selectionStart = s1;
-        text.selectionEnd = s2;
-    } else {
-        text.selectionStart = s2;
-        text.selectionEnd = s1;
-    }
-};
-
-const getTextSelection = () => {
-    return [text.selectionStart, text.selectionEnd]
-};
-
-text.addEventListener("select", (e) => {
-    console.log(e);
-    setTextSelection(start, end);
-});
-
-text.addEventListener("input", (e) => {
-    text.value = prev_value;
-    // console.log(text.selectionStart);
-    // console.log(text.selectionEnd);
-    text.selectionStart = start;
-    text.selectionEnd = end;
-    // e.preventDefault();
-    // console.log(e);
-    // console.log(prev_value);
-    // if (e.data !== null) {
-    //     console.log(e.data.length);
-    // }
-    // console.log(comp);
-
-    // if (e.isComposing) {
-    //     console.log(e.data);
-    //     text.value = prev_value;
-    //     if (e.data.length == 1) {
-    //         comp = false;
-    //     } else {
-    //         comp = true;
-    //     }
-    // }
-    // else if (VALID_VALUES.includes(e.data)) {
-    //     prev_value = text.value;
-    //     console.log("有効な入力");
-    //     comp = false;
-    // }
-    // else if (DELETE_TYPES.includes(e.inputType)) {
-    //     console.log("わーい");
-    //     if (!comp) {
-    //         console.log("削除");
-    //         prev_value = text.value;
-    //     } else {
-    //         console.log("削除無効");
-    //         text.value = prev_value;
-    //     }
-    // }
-    // else {
-    //     text.value = prev_value;
-    //     comp = false;
-    // }
-});
-
-text.addEventListener("keydown", (e) => {
-    let mae, ato;
-    console.log(e);
-    if (VALID_CODES.includes(e.code)) {
-        [start, end] = getTextSelection();
-        console.log(start, end);
-
-        mae = prev_value.slice(0, start);
-        ato = prev_value.slice(end, prev_value.length);
-
-        if (e.code == "Digit0") {
-            text.value = mae + "0" + ato;
-            setTextSelection(start + 1, start + 1);
-        } else if (e.code == "Digit1") {
-            text.value = mae + "1" + ato;
-            setTextSelection(start + 1, start + 1);
-        } else if (e.code == "Backspace") {
-            if (start == end) {
-                text.value = mae.slice(0, -1) + ato;
-                if (start > 0) {
-                    setTextSelection(start - 1, start - 1);
-                }
-            } else {
-                text.value = mae + ato;
-                setTextSelection(start, start);
-            }
-        } else if (e.code == "Delete") {
-            if (start == end) {
-                text.value = mae + ato.slice(1, ato.length);
-            } else {
-                text.value = mae + ato;
-            }
-            setTextSelection(start, start);
-        } else if (e.code == "ArrowLeft") {
-            if (e.shiftKey) {
-                if (start == end) {
-                    base = start;
-                    setTextSelection(start - 1, base);
-                } else if (base < end) {
-                    setTextSelection(base, end - 1);
-                } else if (start > 0) {
-                    setTextSelection(start - 1, base);
-                }
-            } else {
-                if (start != end) {
-                    setTextSelection(start, start);
-                } else if (start > 0) {
-                    setTextSelection(start - 1, start - 1);
-                }
-            }
-        } else if (e.code == "ArrowRight") {
-            if (e.shiftKey) {
-                if (start == end) {
-                    base = start;
-                    setTextSelection(base, end + 1);
-                } else if (start < base) {
-                    setTextSelection(start + 1, base);
-                }
-                else if (start > 0) {
-                    setTextSelection(base, end + 1);
-                }
-            } else {
-                if (start != end) {
-                    setTextSelection(end, end);
-                } else if (start < prev_value.length) {
-                    setTextSelection(start + 1, start + 1);
-                }
-            }
-        } else if (e.code == "KeyA") {
-            if (e.ctrlKey) {
-                setTextSelection(0, prev_value.length);
-            }
-        }
-        [start, end] = getTextSelection();
-        prev_value = text.value;
-    } else {
-        text.value = prev_value;
-    }
-    e.preventDefault();
-});
-
-text.addEventListener("click", (e) => {
-    console.log(e);
-    [start, end] = getTextSelection();
-});
 
 password.addEventListener("input", (e) => {
     password.type = "text";
@@ -200,6 +149,8 @@ function getAllEvents(element) {
 }
 
 // let all_events = getAllEvents(text);
+// console.log(all_events);
+
 // for (const event of all_events) {
 //     console.log(event);
 //     text.addEventListener(event, (e) => {
