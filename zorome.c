cf2b241f4b28900c23f754b274126e015ed76d28
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-02 17:26:43 +0900

    大量文字列の例外処理

--- zorome.c
+++ zorome.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 int main(void) {
     char c;
+    char date_str[3];
+    int date_int;
+    
     printf("日付を入力してください (MM/DD): ");
-    if (!isdigit(getchar())) {
-        puts("数値でない");
+    for (int i = 0; i < 2; i++) {
+        if (!isdigit(getchar())) {
+            return -1;
+        }
+    }
+    if (getchar() != '/') {
         return -1;
     }
-    return 0;
+    for (int i = 0; i < 2; i++) {
+        if (!isdigit(c = getchar())) {
+            return -1;
+        }
+        date_str[i] = c;
+    }
+    date_str[2] = '\0';
+    if (getchar() != '\n') {
+        while (getchar() != '\n');
+        return -1;
+    }
 
+    printf("%s\n", date_str);
+    return 0;
 }
