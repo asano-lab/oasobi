827df65fd76baadfc2a02bb5e0ea5e759510e1da
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-16 14:45:47 +0900

    なぜかエスケープが2つ必要だった

--- svg_test.js
+++ svg_test.js
@@ -1,11 +1,11 @@
 e1 = document.getElementsByClassName("com.sun.star.drawing.CustomShape");
 
-let re = new RegExp('rgb\(\d+,\d+,\d+\)', "i");
+let re = new RegExp("rgb\\(.*\\)", "i");
 
 const rect_fill = document.getElementsByClassName("rect_fill");
 
 for (const i of rect_fill) {
     console.log(i);
     console.log(i.outerHTML);
-    console.log(i.outerHTML.replace(re, "fill=\"rgb(0,255,0)\""));
+    console.log(i.outerHTML.replace(re, "rgb(0,255,0)"));
 }
