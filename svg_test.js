930c52d4b3cc2b5a7b7fcb7def532c0233a57af8
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-18 15:02:42 +0900

    domcontentloadedを使ってみる

--- svg_test.js
+++ svg_test.js
@@ -1,6 +1,21 @@
 const X_NUM = 4;
 const Y_NUM = 3;
 
+let BLOCK_OFFSET_X = 3400;
+let BLOCK_OFFSET_Y = 3400;
+
+let BLOCK_SIZE_X = 2600;
+let BLOCK_SIZE_Y = 3600;
+
+function init() {
+    for (const i of squares) {
+        // console.log(i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\""));
+        i.innerHTML = i.innerHTML.replace(re, "fill=\"rgb(255,255,0)\"");
+        console.log(i.innerHTML);
+        console.log(i);
+    }
+    console.log(squares.length);
+}
 
 e1 = document.getElementsByClassName("com.sun.star.drawing.CustomShape");
 
@@ -8,11 +23,4 @@ let re = new RegExp("fill=\"rgb\\(\\d+,\\d+,\\d+\\)\"", "i");
 
 const squares = document.getElementsByClassName("squares");
 
-console.log(squares.length)
-
-for (const i of squares) {
-    // console.log(i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\""));
-    i.innerHTML = i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\"");
-    console.log(i.innerHTML);
-    console.log(i);
-}
+document.addEventListener("DOMContentLoaded", init);
