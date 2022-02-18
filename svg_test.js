6bec756fea6f4aa350addfa90870f9be4d8b2894
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-18 14:40:02 +0900

    少し簡略化できそう

--- svg_test.js
+++ svg_test.js
@@ -1,12 +1,14 @@
 e1 = document.getElementsByClassName("com.sun.star.drawing.CustomShape");
 
-let re = new RegExp("rgb\\(.*\\)", "i");
+let re = new RegExp("fill=\"rgb\\(\\d+,\\d+,\\d+\\)\"", "i");
 
-const rect_fill = document.getElementsByClassName("rect_fill");
+const squares = document.getElementsByClassName("squares");
 
-for (const i of rect_fill) {
+for (const i of squares) {
     console.log(i);
-    console.log(i.outerHTML);
-    i.outerHTML = i.outerHTML.replace(re, "rgb(0,255,0)");
+    console.log(i.innerHTML);
+    // console.log(i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\""));
+    i.innerHTML = i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\"");
+    console.log(i.innerHTML);
     console.log(i);
 }
