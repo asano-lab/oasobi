8e8b29539dff9e51684a20d58681c060444434f0
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-18 14:53:35 +0900

    jsでsvgを作りたい

--- svg_test.js
+++ svg_test.js
@@ -4,9 +4,9 @@ let re = new RegExp("fill=\"rgb\\(\\d+,\\d+,\\d+\\)\"", "i");
 
 const squares = document.getElementsByClassName("squares");
 
+console.log(squares.length)
+
 for (const i of squares) {
-    console.log(i);
-    console.log(i.innerHTML);
     // console.log(i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\""));
     i.innerHTML = i.innerHTML.replace(re, "fill=\"rgb(0,255,0)\"");
     console.log(i.innerHTML);
