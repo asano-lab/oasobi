ca2fd3f7c1b11f234b66ad9acccc672d8ac4a8b1
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-25 20:53:52 +0900

    最初に改行が連続したときがタイトルかもしれない

--- typing_tube/test001.py
+++ typing_tube/test001.py
@@ -1,3 +1,10 @@
 import pyperclip
+import re
 
-print(pyperclip.paste())
+if __name__ == "__main__":
+    moji = pyperclip.paste()
+    for i in moji.split("\r\n"):
+        m = re.match(r'\d+/\d+', i)
+        if m is not None:
+            pass
+        print(i)
