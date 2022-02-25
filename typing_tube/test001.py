cd82a4f29a89dde7c8dcec2073c56071ba26e962
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-25 21:00:56 +0900

    タイトル取得成功w

--- typing_tube/test001.py
+++ typing_tube/test001.py
@@ -2,14 +2,15 @@ import pyperclip
 import re
 
 if __name__ == "__main__":
-    c = 0
-    c_max = 0
+    prev_title_flag = False
     moji = pyperclip.paste()
     for i in moji.split("\r\n"):
         m = re.match(r'\d+/\d+', i)
         if m is not None:
             pass
-        if i == "":
-            pass
-        else:
+        if i == "https://policies.google.com/privacy":
+            prev_title_flag = True
+        elif prev_title_flag and i:
+            prev_title_flag = False
             print(i)
+        # print(i)
