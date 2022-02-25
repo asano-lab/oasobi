492de6bcbc204e3846b8e34e6df1617f3666cfcc
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-02-25 20:55:21 +0900

    空白行無視

--- typing_tube/test001.py
+++ typing_tube/test001.py
@@ -2,9 +2,14 @@ import pyperclip
 import re
 
 if __name__ == "__main__":
+    c = 0
+    c_max = 0
     moji = pyperclip.paste()
     for i in moji.split("\r\n"):
         m = re.match(r'\d+/\d+', i)
         if m is not None:
             pass
-        print(i)
+        if i == "":
+            pass
+        else:
+            print(i)
