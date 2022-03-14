f192e371702dff11e025c0a7a2b01225f6ba66f1
keiichiro-sonoda <mountain13577749@gmail.com>
2022-03-14 13:15:39 +0900

    fibo.py変更

--- fibo.py
+++ fibo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import time
+import sys
 
 # フィボナッチ数列
 def fibo(n):
@@ -9,7 +10,8 @@ def fibo(n):
         return 1
     return fibo(n - 1) + fibo(n - 2)
 
-n = 37
+n = 45
+print(sys.version)
 t0 = time.time()
 print("fibo(%d) = %d" % (n, fibo(n)))
 print(time.time() - t0)
