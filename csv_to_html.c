e9030b0250ef07b6a3797a03173b79396fa41562
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-04-14 16:39:24 +0900

    編集

--- csv_to_html.c
+++ csv_to_html.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    FILE *fp;
+    FILE *fpr;
     char c;
+    int status = 0;
 
-    if (argc != 3) {
+    if (argc != 2) {
         return -1;
     }
 
-    if ((fp = fopen(argv[1], "r")) == NULL) {
+    if ((fpr = fopen(argv[1], "r")) == NULL) {
         printf("\a%s file open failed\n", argv[1]);
         return -1;
     }
 
-    c = getc(fp);
+    c = getc(fpr);
     while (c != EOF) {
-        c = getc(fp);
-        putchar(c);
+        // putchar(c);
+        switch (status) {
+            case 0:
+                if (c == '"') {
+                    status = 1;
+                } else {
+                    status = 2;
+                }
+                break;
+            case 1:
+                if (c == '"') {
+                    status = 0;
+                }
+                break;
+            case 2:
+
+                break;
+            default:
+                ;
+        }
+        // printf("%d", status);
+        printf("%x ", c);
+        // putchar(c);
+        c = getc(fpr);
     }
 
-    fclose(fp);
+    fclose(fpr);
     return 0;
 }
