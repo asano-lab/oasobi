3cb095984fd82ae787a60338a2f0955ae736ff90
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-04-26 16:45:06 +0900

    とりあえずbasic認証突破

--- selenium_test.py
+++ selenium_test.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 import time
 
 def main():
-    print(webdriver)
+    # print(webdriver)
 
     driver = webdriver.Chrome("C:\FreeSoft\chromedriver_win32\chrome100\chromedriver.exe")
 
-    driver.get("https://www.google.co.jp")
+    # driver.get("https://www.google.co.jp")
+    driver.get("http://ece:Ue%2B%25%24R%2Fg%3D%2C@www-comm.cs.shinshu-u.ac.jp/coding/")
     print(driver)
 
+    # time.sleep(1)
+    # print(Alert(driver).text)
+
+    # driver.switch_to.alert.authenticate("cheese", "secretGouda")
+    # Alert(driver).accept()
 
     time.sleep(5)
 
