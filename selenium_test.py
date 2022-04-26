69856768ec6dc9403c0d5323ddbe76d0361d7dee
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-04-26 16:53:47 +0900

    問題のページまで進んだ

--- selenium_test.py
+++ selenium_test.py
@@ -8,11 +8,17 @@ def main():
     driver = webdriver.Chrome("C:\FreeSoft\chromedriver_win32\chrome100\chromedriver.exe")
 
     # driver.get("https://www.google.co.jp")
+
+    # Percent-encoding
     driver.get("http://ece:Ue%2B%25%24R%2Fg%3D%2C@www-comm.cs.shinshu-u.ac.jp/coding/")
     print(driver)
 
-    # time.sleep(1)
+    time.sleep(1)
     # print(Alert(driver).text)
+
+    quiz1 = driver.find_element_by_xpath("/html/body/div[2]/ol/li[1]/p/table/tbody/tr[2]/td[3]/a")
+
+    quiz1.click()
 
     # driver.switch_to.alert.authenticate("cheese", "secretGouda")
     # Alert(driver).accept()
