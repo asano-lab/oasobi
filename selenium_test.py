d9061e45a12636987176adb335c8cd6bcbf5a849
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-04-26 16:27:40 +0900

    ドライバのバージョンを変更

--- selenium_test.py
+++ selenium_test.py
@@ -1,3 +1,16 @@
 from selenium import webdriver
+import time
 
-print(webdriver)
+def main():
+    print(webdriver)
+
+    driver = webdriver.Chrome("C:\FreeSoft\chromedriver_win32\chrome100\chromedriver.exe")
+
+    driver.get("https://www.google.co.jp")
+    print(driver)
+
+
+    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
