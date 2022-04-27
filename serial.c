338af8c8e9d5399da016e66624b90e2c6b1aec69
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-04-27 21:05:16 +0900

    buffer作成

--- serial.c
+++ serial.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define Num_Terminal 2
 #define Num_Transpoder 1
 
+u_char Buffer_T_Terminal[Num_Terminal][64];
+
 int main(void) {
     printf("%d %d\n", Num_Terminal, Num_Transpoder);
+    printf("%ld\n", sizeof Buffer_T_Terminal);
     return 0;
 }
