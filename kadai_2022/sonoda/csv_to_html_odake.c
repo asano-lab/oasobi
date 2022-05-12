5c6333d5bb7be358483a061b508b657c65e4a084
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-05-12 13:41:45 +0900

    ファイル追加

---
+++ kadai_2022/sonoda/csv_to_html_odake.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+int main(int argc, char *argv[])
+{
+    FILE *csv_file;
+    FILE *html_file;
+    char *csv_file_name = "sample.csv";
+    char *html_file_name = "sample.html";
+    char char_data;
+    char write_text_first[] = "<!DOCTYPE html>\n<html>\n\t<table border=\"1\">\n";
+    char write_text_end[] = "\t</table>\n</html>";
+    char d_flag = false;   //ダブルクォーテーションが開いているかどうかを判断する
+    char d_flag_c = false; //ダブルクォーテーションが連続しているかどうかを判断する
+
+    if (argc == 3)
+    {
+        csv_file_name = argv[1];
+        html_file_name = argv[2];
+    }
+    else if (argc != 1)
+    {
+        printf("引数が正しくありません。\n");
+    }
+    printf("入力ファイル：%s、出力ファイル：%s\n", csv_file_name, html_file_name);
+
+    /////////////////////////////////////////////////////////
+    //ファイルを開く
+    /////////////////////////////////////////////////////////
+    csv_file = fopen(csv_file_name, "r");
+    html_file = fopen(html_file_name, "w");
+    if (csv_file == NULL)
+    {
+        printf("%s が開けませんでした。\n", csv_file_name);
+        return -1;
+    }
+    else if (html_file == NULL)
+    {
+        printf("%s が開けませんでした。\n", html_file_name);
+        return -1;
+    }
+    else
+    {
+        printf("ファイルを正常に開くことができました\n");
+    }
+    /////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////
+    //ファイルに書き込む
+    /////////////////////////////////////////////////////////
+    fprintf(html_file, "%s", write_text_first); //表の前までを書き込む
+
+    fprintf(html_file, "\t<tr>\n\t\t<td>");
+    while (true)
+    {
+        char_data = getc(csv_file);
+        if (char_data == EOF)
+        {
+            fprintf(html_file, "</td>\n\t</tr>\n");
+            break;
+        }
+        else if (char_data == ',' && d_flag == false)
+        {
+            fprintf(html_file, "</td>\n\t\t<td>");
+            d_flag_c = false;
+        }
+        else if (char_data == '\n')
+        {
+            fprintf(html_file, "</td>\n\t</tr>\n\t<tr>\n\t\t<td>");
+            d_flag_c = false;
+        }
+        else if (char_data == '\"')
+        {
+            d_flag = !d_flag;
+            if (d_flag_c == true)
+            {
+                fprintf(html_file, "%c", char_data);
+                d_flag_c = false;
+            }
+            else
+            {
+                d_flag_c = true;
+            }
+        }
+        else
+        {
+            fprintf(html_file, "%c", char_data);
+            d_flag_c = false;
+        }
+    }
+
+    fprintf(html_file, "%s", write_text_end); //表の後ろを書き込む
+    /////////////////////////////////////////////////////////
+
+    fclose(csv_file);
+    fclose(html_file);
+
+    printf("終了しました\n");
+}
---
+++ kadai_2022/sonoda/csv_to_html_urano.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char *string;
+	char cell[256];
+	int cnt;
+	int c = 1;
+	int i;
+	int j;
+
+	FILE *fin = fopen("test.csv","rt");
+	if (!fin){
+		perror("fopen");
+		return 1;
+	}
+	FILE *fin_html = fopen("test.html","at+");
+	if (!fin_html){
+		perror("fopen");
+		return 1;
+	}
+	fprintf(fin_html,"<table border=\"1\">\n",256);
+	while (fgets(string,256,fin)!=NULL){
+		fprintf(fin_html,"<tr>\n",256);
+        cnt=0;
+		for (i=0;string[i]!=0x00;i++){
+			if (string[i]== '\"'){
+				if (string[i+1]!= '\"'){
+					if (c){
+						c=0;
+						cnt=i+1;
+					} else{
+						c=1;
+					}
+				} else{
+					i++;
+				}
+			}
+			if (string[i]==','){
+				if (c){
+					fprintf(fin_html,"<td>\n",256);
+					if (i!=0 || cnt>0){
+					    for (j=0;j+cnt<i;j++){
+					    	if(string[j+cnt]=='\"'){
+					    		if(string[j+cnt+1]=='\"'){
+					    			cell[j]=string[j+cnt];
+								    cnt++;
+								}
+							}else{
+								cell[j]=string[j+cnt];
+							}
+		                }
+						if(cnt!=i){
+							fprintf(fin_html,cell,256);
+						}
+					    cnt=i+1;
+					    memset(cell, '\0', sizeof(cell));
+				    }
+				    fprintf(fin_html,"</td>\n",256);
+				}
+			}
+		}
+		fprintf(fin_html,"<td>\n",256);
+		if(cnt!=i){
+			for (j=0;j+cnt<i-1;j++){
+					    	if(string[j+cnt]=='\"'){
+					    		if(string[j+cnt+1]=='\"'){
+					    			cell[j]=string[j+cnt];
+								    cnt++;
+								}
+							}else{
+								cell[j]=string[j+cnt];
+							}
+		                }
+						if(cnt!=i-1){
+							fprintf(fin_html,cell,256);
+						}
+					    cnt=i+1;
+					    memset(cell, '\0', sizeof(cell));
+		}
+		fprintf(fin_html,"</td>\n",256);
+		fprintf(fin_html,"</tr>\n",256);
+	}
+	fprintf(fin_html,"</table>",256);
+	fclose(fin);
+	fclose(fin_html);
+	return 0;
+}
