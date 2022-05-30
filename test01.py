039b0cf0a8a8b53b8dd476c35a65f6c9676c0446
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-05-30 12:34:44 +0900

    3次元プロットサンプル

--- test01.py
+++ test01.py
@@ -1,11 +1,42 @@
-a = " "
+# PYTHON_MATPLOTLIB_3D_PLOT_01
 
-if a:
-    print("a is not empty")
-else:
-    print("a is empty or None")
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-if a is None:
-    print("a is None")
-else:
-    print("a is string")
+# Figureを追加
+fig = plt.figure(figsize = (8, 8))
+
+# 3DAxesを追加
+ax = fig.add_subplot(111, projection='3d')
+
+# Axesのタイトルを設定
+ax.set_title("Helix", size = 20)
+
+# 軸ラベルを設定
+ax.set_xlabel("x", size = 14)
+ax.set_ylabel("y", size = 14)
+ax.set_zlabel("z", size = 14)
+
+# 軸目盛を設定
+ax.set_xticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+ax.set_yticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+
+# 円周率の定義
+pi = np.pi
+
+# パラメータ分割数
+n = 256
+
+# パラメータtを作成
+t = np.linspace(-6*pi, 6*pi, n)
+
+# らせんの方程式
+x = np.cos(t)
+y = np.sin(t)
+z = t
+
+# 曲線を描画
+ax.plot(x, y, z, color = "red")
+
+plt.show()
