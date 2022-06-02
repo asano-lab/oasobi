99dc55159956a3819763eab6b793108e82a90d19
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-02 15:58:13 +0900

    urlエンコード用のプログラムを追加

--- coding_solver/url_encode.py
+++ coding_solver/url_encode.py
@@ -1,8 +1,9 @@
-import urllib
+import urllib.parse
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="パーセントエンコーディング")
     parser.add_argument("string", help="変換したい文字列")
     args = parser.parse_args()
-    print(args.string)
+
+    print(urllib.parse.quote(args.string, safe=""))
