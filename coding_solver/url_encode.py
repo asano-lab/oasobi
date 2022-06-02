9223ddf00a2c8f55d2357806c0fcc31dd2cb4d6e
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-02 15:37:48 +0900

    ファイル追加

---
+++ coding_solver/url_encode.py
@@ -0,0 +1,8 @@
+import urllib
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="パーセントエンコーディング")
+    parser.add_argument("string", help="変換したい文字列")
+    args = parser.parse_args()
+    print(args.string)
