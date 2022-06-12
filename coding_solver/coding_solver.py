d97700c2afb85114458ee8fbbf5a74c04856fff8
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-13 08:25:35 +0900

    フォーマット

--- coding_solver/coding_solver.py
+++ coding_solver/coding_solver.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="符号化技術特論基礎編の課題を解くプログラム")
     parser.add_argument("username", help="ユーザー名")
     parser.add_argument("studentnumber", help="学籍番号")
-    parser.add_argument("-c", "--chapter", help="章 (1, 2, 3-1, 3-2,...)", nargs="+")
+    parser.add_argument("-c", "--chapter",
+                        help="章 (1, 2, 3-1, 3-2,...)", nargs="+")
 
-    parser.add_argument("-b", "--browser", help="ブラウザ (Chrome, Edge, Firefox)", type=str, default="Edge")
+    parser.add_argument(
+        "-b", "--browser", help="ブラウザ (Chrome, Edge, Firefox)", type=str, default="Edge")
     parser.add_argument("--repeat", help="繰り返して解く回数", type=int, default=1)
 
     args = parser.parse_args()
@@ -26,10 +28,14 @@ SLEEP_TIME = 0
 
 RETRY_MAX = 0
 
+
 def solve_questions(driver, chapter):
-    e1 = driver.find_element(by=By.XPATH, value="/html/body/dir[1]/table/tbody/tr/td/blockquote")
-    answer_input = driver.find_element(by=By.XPATH, value="/html/body/dir[2]/form/input")
-    answer_button = driver.find_element(by=By.XPATH, value="/html/body/input[5]")
+    e1 = driver.find_element(
+        by=By.XPATH, value="/html/body/dir[1]/table/tbody/tr/td/blockquote")
+    answer_input = driver.find_element(
+        by=By.XPATH, value="/html/body/dir[2]/form/input")
+    answer_button = driver.find_element(
+        by=By.XPATH, value="/html/body/input[5]")
 
     inner_html = e1.get_attribute("innerHTML")
 
@@ -41,28 +47,35 @@ def solve_questions(driver, chapter):
             answer_input.send_keys(q["solver"](m))
             answer_button.click()
             return True
-    
+
     print("合致する問題がありません")
     return False
 
+
 def complete_questions(driver, chapter, username, studentnumber):
-    quiz_link = driver.find_element(by=By.XPATH, value=QUESTIONS[chapter]["xpath"])
+    quiz_link = driver.find_element(
+        by=By.XPATH, value=QUESTIONS[chapter]["xpath"])
     quiz_link.click()
 
     time.sleep(SLEEP_TIME)
 
-    username_input = driver.find_element(by=By.XPATH, value="/html/body/dl/dd[2]/form/table/tbody/tr[1]/td[2]/input")
+    username_input = driver.find_element(
+        by=By.XPATH, value="/html/body/dl/dd[2]/form/table/tbody/tr[1]/td[2]/input")
     username_input.send_keys(username)
 
-    studentnumter_input = driver.find_element(by=By.XPATH, value="/html/body/dl/dd[2]/form/table/tbody/tr[2]/td[2]/input")
+    studentnumter_input = driver.find_element(
+        by=By.XPATH, value="/html/body/dl/dd[2]/form/table/tbody/tr[2]/td[2]/input")
     studentnumter_input.send_keys(studentnumber)
 
-    start_button = driver.find_element(by=By.XPATH, value="/html/body/dl/dd[2]/form/input[5]")
+    start_button = driver.find_element(
+        by=By.XPATH, value="/html/body/dl/dd[2]/form/input[5]")
     start_button.click()
 
     time.sleep(SLEEP_TIME)
 
-    start_button = driver.find_element(by=By.XPATH, value="/html/body/center/form[1]/input[8]")
+    start_button = driver.find_element(
+        by=By.XPATH, value="/html/body/center/form[1]/input[8]")
+    input(f"ユーザ名: {username}\n学籍番号: {studentnumber}\nが正しければEnterを押してください")
     start_button.click()
 
     cleared = True
@@ -74,11 +87,13 @@ def complete_questions(driver, chapter, username, studentnumber):
         if cleared:
             time.sleep(SLEEP_TIME)
             try:
-                next_button = driver.find_element(by=By.XPATH, value="/html/body/blockquote/dir/form/input[5]")
+                next_button = driver.find_element(
+                    by=By.XPATH, value="/html/body/blockquote/dir/form/input[5]")
                 next_button.click()
             except NoSuchElementException:
                 try:
-                    back_button = driver.find_element(by=By.XPATH, value="/html/body/blockquote/p/a")
+                    back_button = driver.find_element(
+                        by=By.XPATH, value="/html/body/blockquote/p/a")
                     back_button.click()
                     print("#%s クリア!!" % chapter)
                     break
@@ -86,13 +101,16 @@ def complete_questions(driver, chapter, username, studentnumber):
                     print("不正解!")
                     input("press enter to end: ")
                     if retry_count < RETRY_MAX:
-                        retry_button = driver.find_element(by=By.XPATH, value="/html/body/center/table[2]/tbody/tr/td[1]/form/input[8]")
+                        retry_button = driver.find_element(
+                            by=By.XPATH, value="/html/body/center/table[2]/tbody/tr/td[1]/form/input[8]")
                         retry_button.click()
                         retry_count += 1
                     else:
-                        back_button = driver.find_element(by=By.XPATH, value="/html/body/center/table[2]/tbody/tr/td[2]/a")
+                        back_button = driver.find_element(
+                            by=By.XPATH, value="/html/body/center/table[2]/tbody/tr/td[2]/a")
                         back_button.click()
                         break
+
 
 def main(username, studentnumber, chapter_list, browser, repeat):
     pf = platform.system()
@@ -146,11 +164,14 @@ def main(username, studentnumber, chapter_list, browser, repeat):
         else:
             for chapter in chapter_list:
                 if chapter in QUESTIONS.keys():
-                    complete_questions(driver, chapter, username, studentnumber)
+                    complete_questions(
+                        driver, chapter, username, studentnumber)
                     time.sleep(SLEEP_TIME)
 
     # input("press enter to end: ")
     # time.sleep(3)
 
+
 if __name__ == "__main__":
-    main(args.username, args.studentnumber, args.chapter, args.browser, args.repeat)
+    main(args.username, args.studentnumber,
+         args.chapter, args.browser, args.repeat)
