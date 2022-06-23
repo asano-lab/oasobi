bbd0d7bce3a4856a7c196f0b5804a3eb2459fe18
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-23 13:21:33 +0900

    とりあえず操作を記録するためのスクリプト作成 流石にパスワードは保存できない

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# 初期設定用ファイル
+# ID, パスワードが平文で保存されるので注意
 
-while read line
-do
-    echo $line
-done
+url="https://login.shinshu-u.ac.jp"
+
+echo "$url で自動ログインするための操作を記録するスクリプト"
+echo "操作を保存するパスを入力"
+read line
+
+lynx $url -cmd_log $line
+
+echo "ログの保存が完了しました."
+echo "以下を実行すれば自動でログインできます."
+echo "lynx \"$url\" -cmd_script $line"
