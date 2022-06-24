aafd58479561dc51964d5230f62b7973853716b4
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-24 15:49:41 +0900

    報酬関数作成

--- gopher.py
+++ gopher.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 class Status():
+    """
+    状態
+    """
+
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
@@ -10,6 +14,10 @@ class Status():
 
 
 class Action():
+    """
+    動作
+    """
+
     def __init__(self, direction: str):
         """
         u, d, r, l
@@ -20,7 +28,20 @@ class Action():
         return self.direction
 
 
+def reward(s_t: Status, a_t: Action, s_tp1: Status) -> float:
+    """
+    報酬
+    """
+    r = 1.0
+    if (s_tp1.x == 2 and s_tp1.y == 2):
+        r = 100.0
+    return r
+
+
 if __name__ == "__main__":
     s0 = Status(0, 1)
     a0 = Action("r")
-    print(s0, a0)
+    s1 = Status(2, 1)
+    print(s0, a0, s1)
+    r = reward(s0, a0, s1)
+    print(r)
