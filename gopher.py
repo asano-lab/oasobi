4edd41952d7885e1c230265bba89e7dc9ba13ad6
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-24 15:58:22 +0900

    動作適用メソッド作成

--- gopher.py
+++ gopher.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-class Status():
-    """
-    状態
-    """
-
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x},{self.y})"
-
-
 class Action():
     """
     動作
@@ -22,10 +9,40 @@ class Action():
         """
         u, d, r, l
         """
-        self.direction = direction
+        self.direction = "u"
+        if direction in ("u", "d", "r", "l"):
+            self.direction = direction
 
     def __str__(self):
         return self.direction
+
+
+class Status():
+    """
+    状態
+    """
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def apply_action(self, a: Action):
+        """
+        動作適用
+        """
+        if a.direction == "u":
+            self.y += 1
+        elif a.direction == "d":
+            self.y -= 1
+        elif a.direction == "r":
+            self.x += 1
+        else:
+            self.x -= 1
+        self.x = max(0, min(2, self.x))
+        self.y = max(0, min(2, self.y))
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
 
 
 def reward(s_t: Status, a_t: Action, s_tp1: Status) -> float:
@@ -41,6 +58,10 @@ def reward(s_t: Status, a_t: Action, s_tp1: Status) -> float:
 if __name__ == "__main__":
     s0 = Status(0, 1)
     a0 = Action("r")
+    s0.apply_action(a0)
+    s0.apply_action(Action("u"))
+    s0.apply_action(Action("d"))
+    s0.apply_action(Action("d"))
     s1 = Status(2, 1)
     print(s0, a0, s1)
     r = reward(s0, a0, s1)
