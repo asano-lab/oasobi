12e4d5a17d43413f44f30de7d82004ee933ea9e4
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-25 14:10:05 +0900

    掃き出し法実装

--- coding/hakidashi.py
+++ coding/hakidashi.py
@@ -7,18 +7,25 @@ G = np.matrix([
 ], dtype="u1")
 
 print(G)
-print(G.dtype)
-print(G.shape)
+# print(G.dtype)
+# print(G.shape)
 
 k = G.shape[0]
 
-A = np.matrix(np.identity(k))
+A = np.matrix(np.identity(k), dtype="u1")
+G_copy = G.copy()
 for i in range(k):
-    col_i = np.array(G)[:,i]
+    col_i = np.array(G_copy)[:,i]
     if col_i[i] == 1:
         A_i = np.identity(k)
         A_i[:,i] = col_i
+        A_i = np.matrix(A_i, dtype="u1")
         A = A_i * A % 2
-        G = A_i * G % 2
+        # print(f"A_i=\n{A_i}")
+        # print(f"A=\n{A}")
+        G_copy = A_i * G_copy % 2
+        print(f"掃き出し{i + 1}回目")
+        print(G_copy)
 
-print(G)
+print(A)
+print(A * G % 2)
