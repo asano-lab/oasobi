7ef43e0355e519028adf9fd04f612b51deca3a83
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-25 13:49:19 +0900

    行列の乗算

--- coding/hakidashi.py
+++ coding/hakidashi.py
@@ -4,6 +4,16 @@ G = np.matrix([
     [1, 0, 1, 0, 1, 1],
     [1, 1, 1, 1, 0, 1],
     [0, 1, 1, 0, 0, 1]
-])
+], dtype="u1")
 
 print(G)
+print(G.dtype)
+print(G.shape)
+
+A = np.matrix([
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 1]
+])
+
+print(A * G % 2)
