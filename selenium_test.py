6f7ab54a9abc88da6b75d0d54e191731fde7cf55
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-19 07:43:53 +0100

    headlessならguiなくても動く

--- selenium_test.py
+++ selenium_test.py
@@ -6,10 +6,16 @@ import time
 # sys.path.append('/home/pi/.local/lib/python3.5/site-packages/')
 from selenium import webdriver
 from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
 
 CHROMEDRIVER = "/usr/lib/chromium-browser/chromedriver"
 CHROME_SERVICE = fs.Service(executable_path=CHROMEDRIVER)
-browser = webdriver.Chrome(service=CHROME_SERVICE)
+browser = webdriver.Chrome(service=CHROME_SERVICE, options=options)
+
+
 
 time.sleep(1)
 browser.quit()
