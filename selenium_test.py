7da67dc5efeadab0a871a3b65701699d0442d320
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-19 07:27:33 +0100

    ブラウザ閉じた

--- selenium_test.py
+++ selenium_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 # sys.path.append('/home/pi/.local/lib/python3.5/site-packages/')
 from selenium import webdriver
 from selenium.webdriver.chrome import service as fs
@@ -9,6 +10,9 @@ from selenium.webdriver.chrome import service as fs
 CHROMEDRIVER = "/usr/lib/chromium-browser/chromedriver"
 CHROME_SERVICE = fs.Service(executable_path=CHROMEDRIVER)
 browser = webdriver.Chrome(service=CHROME_SERVICE)
+
+time.sleep(1)
+browser.quit()
 
 # browser = webdriver.Chrome()
 # browser.get("https://www.google.com/")
