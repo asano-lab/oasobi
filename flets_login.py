0214390be882907f705c7812feee5ec20e34111e
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-23 17:54:51 +0900

    ファイルコピー wikiの動作が毎回一定とは限らない?

---
+++ flets_login.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
+
+CHROMEDRIVER = "/usr/lib/chromium-browser/chromedriver"
+CHROME_SERVICE = fs.Service(executable_path=CHROMEDRIVER)
+
+try:
+    # GUIが使える場合
+    browser = webdriver.Chrome(service=CHROME_SERVICE)
+except WebDriverException:
+    # GUIが使えない場合
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Chrome(service=CHROME_SERVICE, options=options)
+
+browser.get("https://www.google.com/")
+
+# q = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+q = browser.find_element(By.NAME, "q")
+q.send_keys("あいうえお\n")
+
+r = browser.find_element(By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/a")
+r.click()
+
+s = browser.find_element(By.XPATH, "/html/body/div/div/div[3]/main/div[2]/div[4]/div[1]/p")
+print(s.get_attribute("innerText"))
+
+time.sleep(1)
+browser.quit()
