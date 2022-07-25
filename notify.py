f25b921c2cbba78e579dfb24d18a9b250a57facc
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-26 00:31:17 +0900

    modified:   notify.py

--- notify.py
+++ notify.py
@@ -5,21 +5,25 @@ import requests
 # 平文で保存してある
 TOKEN_PATH = "/home/sonoda/.secret/line_token01.txt"
 
+LINE_NOTIFY_API = "https://notify-api.line.me/api/notify"
+
+
 def main():
     with open(TOKEN_PATH, "r", encoding="UTF-8") as f:
         line_token = f.read().split("\n")[1]
     print(line_token)
     # send_line_notify("てすとてすと", line_token)
 
+
 def send_line_notify(notification_message, token):
     """
     LINEに通知する
     """
     line_notify_token = token
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: {notification_message}'}
-    requests.post(line_notify_api, headers = headers, data = data)
+    headers = {"Authorization": f"Bearer {line_notify_token}"}
+    data = {"message": f"message: {notification_message}"}
+    requests.post(LINE_NOTIFY_API, headers=headers, data=data)
+
 
 if __name__ == "__main__":
     main()
