bc24e4eededf981ba69abf8064df018460966d28
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-26 00:34:25 +0900

    日付送信

--- notify.py
+++ notify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import datetime
 
 # 平文で保存してある
 TOKEN_PATH = "/home/sonoda/.secret/line_token01.txt"
@@ -12,7 +13,9 @@ def main():
     with open(TOKEN_PATH, "r", encoding="UTF-8") as f:
         line_token = f.read().split("\n")[1]
     print(line_token)
-    # send_line_notify("てすとてすと", line_token)
+    send_line_notify(
+        datetime.datetime.now().strftime("%Y%m%d%H%M%S"), line_token
+    )
 
 
 def send_line_notify(notification_message, token):
