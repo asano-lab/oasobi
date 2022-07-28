5bea2d105da2d822b8a584ed49bba2465ba59b74
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-28 15:26:12 +0900

    間接的にサンプル数を指定

--- coding/experiment.py
+++ coding/experiment.py
@@ -5,14 +5,6 @@ import argparse
 import time
 import numpy as np
 
-def fibo(x):
-    if x <= 0:
-        y = 0
-    elif x == 1:
-        y = 1
-    else:
-        y = fibo(x - 2) + fibo(x - 1)
-    return y
 
 class MyThread(threading.Thread):
     def __init__(self, e_prob, count, name=""):
@@ -27,21 +19,23 @@ class MyThread(threading.Thread):
         subprocess.run(["./experiment", self.fname, f"{self.e_prob}", f"{self.count}"])
 
 def main():
-    th_list = []
-    t0 = time.time()
-    for i in range(1):
-        tmp_th = MyThread(0.01, 1000000, str(i))
-        tmp_th.run()
-    while threading.active_count() > 1:
-        time.sleep(1)
-    print(time.time() - t0)
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="符号の実験")
     parser.add_argument("-c", "--count", help="ループ数", type=int, default=10000)
     args = parser.parse_args()
 
     subprocess.run(["make", "experiment"])
+
+    th_list = []
+    t0 = time.time()
+    for i in range(1):
+        tmp_th = MyThread(0.01, args.count, str(i))
+        tmp_th.run()
+        th_list.append(tmp_th)
+    while threading.active_count() > 1:
+        time.sleep(1)
+    print(time.time() - t0)
+
+if __name__ == "__main__":
     # print(args.count)
     main()
     # print(np.logspace(-6, -0.5, 12))
