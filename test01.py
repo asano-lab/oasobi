ad4b694e6724d92d61ee7e5d2a9836ab198818a9
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-28 18:27:02 +0900

    コメントアウト

--- test01.py
+++ test01.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import datetime
-import numpy as np
-import pandas as pd
 from pytz import timezone
 import numpy as np
 
@@ -10,8 +8,8 @@ TZ_TOKYO = timezone("Asia/Tokyo")
 
 def main():
     t0 = datetime.datetime.now(tz=TZ_TOKYO)
-    print(t0)
-    print(t0.tzinfo)
+    # print(t0)
+    # print(t0.tzinfo)
     print(t0.strftime("%a, %d %b %Y %H:%M:%S %z"))
 
 
