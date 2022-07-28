d250e3c430811aa2bd820ffb248e2dee5938ea21
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-28 20:25:27 +0900

    インターネット接続を確認できた?

--- test01.py
+++ test01.py
@@ -2,12 +2,17 @@
 import datetime
 from pytz import timezone
 import pings
+import urllib.request
+from urllib.error import URLError
 
-p = pings.Ping()  # Pingオブジェクト作成
-res = p.ping("google.com")  # googleを監視
 
-res.print_messages()
-print(res.is_reached())
+def internet_on():
+    try:
+        urllib.request.urlopen("https://www.google.com", timeout=1)
+        return True
+    except URLError:
+        return False
+
 
 TZ_TOKYO = timezone("Asia/Tokyo")
 
@@ -17,6 +22,7 @@ def main():
     # print(t0)
     # print(t0.tzinfo)
     print(t0.strftime("%a, %d %b %Y %H:%M:%S %z"))
+    print(internet_on())
 
 
 if __name__ == "__main__":
