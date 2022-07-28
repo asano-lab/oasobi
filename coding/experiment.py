312232bb008fb8c3e14c75f8279b48abe45b7248
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-28 14:31:50 +0900

    pythonのthreading微妙

--- coding/experiment.py
+++ coding/experiment.py
@@ -24,14 +24,14 @@ class MyThread(threading.Thread):
 def main():
     th_list = []
     t0 = time.time()
-    # fibo(30)
     for _ in range(100):
-        tmp_th = MyThread()
-        tmp_th.start()
-        th_list.append(tmp_th)
-        # print(end=f"{threading.active_count()},", flush=True)
-    while threading.active_count() > 1:
-        time.sleep(0.1)
+        print(fibo(30))
+    #     tmp_th = MyThread()
+    #     tmp_th.start()
+    #     th_list.append(tmp_th)
+    #     # print(end=f"{threading.active_count()},", flush=True)
+    # while threading.active_count() > 1:
+    #     time.sleep(0.1)
     print()
     print(time.time() - t0)
 
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="符号の実験")
     parser.add_argument("-c", "--count", help="ループ数", type=int, default=10000)
     args = parser.parse_args()
-    print(args.count)
+    # print(args.count)
     main()
