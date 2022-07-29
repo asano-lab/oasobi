90e13b08878b6f2dd2284d0e778c57919b3306fd
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-29 13:56:50 +0900

    new file:   analysis.py

---
+++ coding/analysis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import glob
+import subprocess
+import threading
+import argparse
+import time
+import numpy as np
+
+
+def main():
+    print(glob.glob("dat/*"))
+
+if __name__ == "__main__":
+    main()
