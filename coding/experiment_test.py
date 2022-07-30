3b9ed510ab1ff614e3cb96155a4a51176a5a084f
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-30 18:37:43 +0900

    1億ループでテスト ループが少数になっていた

--- coding/experiment_test.py
+++ coding/experiment_test.py
@@ -8,15 +8,16 @@ def main():
     subprocess.run(["make", "experiment"])
 
     p_bsc = np.power(10, -4 - 1 / 3)
-    ev = 2000000
+    ev = 20000
     count = ev / p_bsc / 4
-    print(p_bsc, ev, count)
-    return
 
-    with open("test.csv", "a", encoding="UTF-8") as f:
-        print()
+    test_path = "test.csv"
+
+    with open(test_path, "a", encoding="UTF-8") as f:
+        print(f"p_bsc={p_bsc},loop={count}", file=f)
+
     t0 = time.time()
-    subprocess.run(["./experiment", "test.csv", "4.64e-5", "20000000000"])
+    subprocess.run(["./experiment", test_path, str(p_bsc), str(count)])
     print(time.time() - t0)
 
 
