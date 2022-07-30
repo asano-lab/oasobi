defeb66fd03125f13d65e9035c2fc0a8ed696e9a
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-30 19:12:21 +0900

    若干確率を上げてみる

--- coding/experiment_test.py
+++ coding/experiment_test.py
@@ -7,12 +7,13 @@ import numpy as np
 def main():
     subprocess.run(["make", "experiment"])
 
-    p_bsc = np.power(10, -4 - 1 / 3)
-    ev = 2000000
+    p_bsc = np.power(10, -4 - 1 / 4)
+    ev = 4000
     count = round(ev / p_bsc / 4)
 
     test_path = "test.csv"
 
+    print(f"p_bsc={p_bsc},loop={count}")
     with open(test_path, "a", encoding="UTF-8") as f:
         print(f"p_bsc={p_bsc},loop={count}", file=f)
 
