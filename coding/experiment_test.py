3bbf1a78708ae69de84045f1cc28f4810ef2aeb6
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-30 18:35:32 +0900

    パラメータ確認

--- coding/experiment_test.py
+++ coding/experiment_test.py
@@ -7,11 +7,18 @@ import numpy as np
 def main():
     subprocess.run(["make", "experiment"])
 
+    p_bsc = np.power(10, -4 - 1 / 3)
+    ev = 2000000
+    count = ev / p_bsc / 4
+    print(p_bsc, ev, count)
+    return
+
+    with open("test.csv", "a", encoding="UTF-8") as f:
+        print()
     t0 = time.time()
-    subprocess.run(["./experiment", "test.csv", "4.64e-6", "20000000000"])
+    subprocess.run(["./experiment", "test.csv", "4.64e-5", "20000000000"])
     print(time.time() - t0)
 
 
 if __name__ == "__main__":
     main()
-    # print(np.power(10, -5-1/3))
