6ba36afe56c590b2e699f53144f0f03c47065305
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-08-21 23:31:05 +0900

    .shで読み込んだ

--- backup_eliminate.py
+++ backup_eliminate.py
@@ -7,13 +7,7 @@ DIRNAME = "/home/sonoda/backups"
 
 fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
 fname_list.sort(key=os.path.getmtime)
-print(fname_list)
 
 # 古い順に削除
 for i in range(len(fname_list) - N):
     os.remove(fname_list[i])
-
-
-fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
-fname_list.sort(key=os.path.getmtime)
-print(fname_list)
