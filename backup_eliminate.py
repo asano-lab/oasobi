8bfd36c17c17edf77a9b647c2d1726b53c005221
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-08-21 23:25:55 +0900

    ちゃんとファイル削除できたっぽい

--- backup_eliminate.py
+++ backup_eliminate.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 import os
 
+N = 3
+
 DIRNAME = "/home/sonoda/backups"
+
+fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
+fname_list.sort(key=os.path.getmtime)
+print(fname_list)
+
+# 古い順に削除
+for i in range(len(fname_list) - N):
+    os.remove(fname_list[i])
+
 
 fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
 fname_list.sort(key=os.path.getmtime)
