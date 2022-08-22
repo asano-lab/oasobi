d6b8cfda82fab4f0eff568019cdffaaa002eb72e
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-08-22 14:30:47 +0900

    ディレクトリ内のすべてのファイルを対象にせず、特定のパターンだけ対象とした

--- backup_eliminate.py
+++ backup_eliminate.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+import glob
 import os
 
 N = 3
 
-DIRNAME = "/home/sonoda/backups"
+BACKUP_PATH_PATTERN = "/home/sonoda/backups/oasobi_backup*"
 
-fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
+fname_list = glob.glob(BACKUP_PATH_PATTERN)
+# fname_list = [os.path.join(DIRNAME, i) for i in os.listdir(DIRNAME)]
+
 fname_list.sort(key=os.path.getmtime)
+# print(fname_list)
+# exit()
 
 # 古い順に削除
 for i in range(len(fname_list) - N):
