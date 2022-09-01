966cbba607016a06605b3503df2f12e2ccfbc38f
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-09-01 17:34:38 +0900

    参照ファイル変更

--- flets_login.py
+++ flets_login.py
@@ -42,7 +42,7 @@ FLETS_LOGIN_URL = "https://wifi.e-flets.jp"
 GOOGLE_URL = "https://www.google.com"
 
 # 平文で保存してある
-TOKEN_PATH = os.path.abspath(os.path.join(PRIVATE_DIR, "line_token01.txt"))
+TOKEN_PATH = os.path.abspath(os.path.join(PRIVATE_DIR, "line_token_ssb.txt"))
 LINE_NOTIFY_API = "https://notify-api.line.me/api/notify"
 
 
