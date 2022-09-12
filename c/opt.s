1e56488cfa03575fb09372820152b2b0d012bb5c
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-09-12 11:48:40 +0900

    アセンブリを追加

---
+++ c/opt.s
@@ -0,0 +1,76 @@
+	.file	"opt.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"usage: %s <number>\n"
+.LC1:
+	.string	"%d > %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	cmpl	$1, -20(%rbp)
+	jg	.L2
+	movq	-32(%rbp), %rax
+	movq	(%rax), %rdx
+	movq	stderr(%rip), %rax
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf@PLT
+	movl	$1, %eax
+	jmp	.L3
+.L2:
+	movq	-32(%rbp), %rax
+	addq	$8, %rax
+	movq	(%rax), %rax
+	movl	$0, %edx
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	strtol@PLT
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	leal	1(%rax), %ecx
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	movl	%ecx, %esi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
