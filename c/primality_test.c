f29d2c8f5061da8bf6eb2dfcbdd41644b35d930f
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-09-12 12:38:41 +0900

    判定対象の平方根以下の素数のみチェックするだけにして時短

--- c/primality_test.c
+++ c/primality_test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/time.h>
+#include <math.h>
 
 #define N 100000
 
@@ -9,7 +10,7 @@ int main(void) {
     struct timeval t0, t1;
     double t0_sec, t1_sec;
 
-    int i, j, n;
+    u_int i, j, n, sqrt_n;
     u_int prime_numbers[N];
     bool prime_flag;
     prime_numbers[0] = 2;
@@ -17,12 +18,16 @@ int main(void) {
     gettimeofday(&t0, NULL);
     i = 1;
     for (n = 3; i < N; n++) {
+        sqrt_n = sqrt(n) + 1;
         // printf("%d: ", n);
         prime_flag = true;
         for (j = 0; j < i; j++) {
             if (n % prime_numbers[j] == 0) {
                 // printf("smallest prime factor is %d\n", prime_numbers[j]);
                 prime_flag = false;
+                break;
+            }
+            if (prime_numbers[j] > sqrt_n) {
                 break;
             }
         }
--- c/primality_test.s
+++ c/primality_test.s
@@ -1,9 +1,9 @@
 	.file	"primality_test.c"
 	.text
 	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC1:
+.LC4:
 	.string	"%d\n"
-.LC2:
+.LC5:
 	.string	"%f\n"
 	.section	.text.startup,"ax",@progbits
 	.p2align 4
@@ -13,51 +13,84 @@ main:
 .LFB39:
 	.cfi_startproc
 	endbr64
+	pushq	%r12
+	.cfi_def_cfa_offset 16
+	.cfi_offset 12, -16
+	pushq	%rbp
+	.cfi_def_cfa_offset 24
+	.cfi_offset 6, -24
+	pushq	%rbx
+	.cfi_def_cfa_offset 32
+	.cfi_offset 3, -32
 	leaq	-397312(%rsp), %r11
-	.cfi_def_cfa 11, 397320
+	.cfi_def_cfa 11, 397344
 .LPSRL0:
 	subq	$4096, %rsp
 	orq	$0, (%rsp)
 	cmpq	%r11, %rsp
 	jne	.LPSRL0
 	.cfi_def_cfa_register 7
-	subq	$2760, %rsp
-	.cfi_def_cfa_offset 400080
+	subq	$2752, %rsp
+	.cfi_def_cfa_offset 400096
 	xorl	%esi, %esi
+	movl	$3, %ebx
+	movl	$1, %ebp
 	movq	%fs:40, %rax
 	movq	%rax, 400056(%rsp)
 	xorl	%eax, %eax
 	leaq	16(%rsp), %rdi
 	movl	$2, 48(%rsp)
+	leaq	48(%rsp), %r12
 	call	gettimeofday@PLT
-	movl	$3, %esi
-	movl	$1, %r8d
-	leaq	48(%rsp), %r10
-	leaq	52(%rsp), %r9
+	movsd	.LC0(%rip), %xmm1
+	pxor	%xmm3, %xmm3
+	movsd	.LC2(%rip), %xmm2
 	.p2align 4,,10
 	.p2align 3
 .L2:
-	leal	-1(%r8), %eax
-	movq	%r10, %rcx
-	leaq	(%r9,%rax,4), %rdi
+	addsd	%xmm2, %xmm1
+	movl	%ebp, %r9d
+	movq	%r12, %rsi
+	leaq	(%r12,%r9,4), %r8
+	cvttsd2siq	%xmm1, %rdi
 	.p2align 4,,10
 	.p2align 3
-.L4:
-	movl	%esi, %eax
+.L8:
+	movl	(%rsi), %ecx
+	movl	%ebx, %eax
 	xorl	%edx, %edx
-	divl	(%rcx)
+	divl	%ecx
 	testl	%edx, %edx
-	je	.L3
-	addq	$4, %rcx
-	cmpq	%rdi, %rcx
-	jne	.L4
-	movslq	%r8d, %rax
-	addl	$1, %r8d
-	movl	%esi, 48(%rsp,%rax,4)
-.L3:
-	addl	$1, %esi
-	cmpl	$100000, %r8d
-	jne	.L2
+	je	.L6
+	cmpl	%edi, %ecx
+	ja	.L7
+	addq	$4, %rsi
+	cmpq	%rsi, %r8
+	jne	.L8
+.L7:
+	movl	%ebx, 48(%rsp,%r9,4)
+	addl	$1, %ebp
+.L6:
+	addl	$1, %ebx
+	cmpl	$100000, %ebp
+	je	.L18
+	movl	%ebx, %eax
+	pxor	%xmm0, %xmm0
+	cvtsi2sdq	%rax, %xmm0
+	ucomisd	%xmm0, %xmm3
+	movapd	%xmm0, %xmm1
+	sqrtsd	%xmm1, %xmm1
+	jbe	.L2
+	movsd	%xmm1, (%rsp)
+	call	sqrt@PLT
+	movq	.LC2(%rip), %rax
+	movsd	(%rsp), %xmm1
+	pxor	%xmm3, %xmm3
+	movq	%rax, %xmm2
+	jmp	.L2
+	.p2align 4,,10
+	.p2align 3
+.L18:
 	xorl	%esi, %esi
 	leaq	32(%rsp), %rdi
 	call	gettimeofday@PLT
@@ -65,9 +98,9 @@ main:
 	pxor	%xmm0, %xmm0
 	xorl	%eax, %eax
 	cvtsi2sdq	24(%rsp), %xmm1
-	movsd	.LC0(%rip), %xmm2
+	movsd	.LC3(%rip), %xmm2
 	movl	400044(%rsp), %edx
-	leaq	.LC1(%rip), %rsi
+	leaq	.LC4(%rip), %rsi
 	cvtsi2sdq	16(%rsp), %xmm0
 	movl	$1, %edi
 	mulsd	%xmm2, %xmm1
@@ -83,19 +116,25 @@ main:
 	call	__printf_chk@PLT
 	movsd	8(%rsp), %xmm0
 	subsd	(%rsp), %xmm0
-	leaq	.LC2(%rip), %rsi
+	leaq	.LC5(%rip), %rsi
 	movl	$1, %edi
 	movl	$1, %eax
 	call	__printf_chk@PLT
 	movq	400056(%rsp), %rax
 	xorq	%fs:40, %rax
-	jne	.L16
-	xorl	%eax, %eax
-	addq	$400072, %rsp
+	jne	.L19
+	addq	$400064, %rsp
 	.cfi_remember_state
+	.cfi_def_cfa_offset 32
+	xorl	%eax, %eax
+	popq	%rbx
+	.cfi_def_cfa_offset 24
+	popq	%rbp
+	.cfi_def_cfa_offset 16
+	popq	%r12
 	.cfi_def_cfa_offset 8
 	ret
-.L16:
+.L19:
 	.cfi_restore_state
 	call	__stack_chk_fail@PLT
 	.cfi_endproc
@@ -104,6 +143,14 @@ main:
 	.section	.rodata.cst8,"aM",@progbits,8
 	.align 8
 .LC0:
+	.long	3898100906
+	.long	1073460858
+	.align 8
+.LC2:
+	.long	0
+	.long	1072693248
+	.align 8
+.LC3:
 	.long	2696277389
 	.long	1051772663
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
