73ea1123df52795b7514dc8b2e8d45b154aeacd5
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-10-24 20:31:20 +0900

    クラス名に変更した

--- selenium_test.py
+++ selenium_test.py
@@ -25,18 +25,16 @@ browser.get("https://www.google.com/")
 q = browser.find_element(By.NAME, "q")
 q.send_keys("あいうえお\n")
 
-# クラス名は変わる場合あり
 element_list = browser.find_elements(By.CLASS_NAME, "LC20lb.MBeuO.DKV0Md")
 for el in element_list:
     if "Wikipedia" in el.get_attribute("innerText"):
         el.click()
         break
 
-# xpathは変わる場合あり
-s = browser.find_element(
-    By.XPATH, "/html/body/div[1]/div/div[4]/main/div[2]/div[3]/div[1]/p")
 
-print(s.get_attribute("innerText"))
+element_list = browser.find_elements(By.CLASS_NAME, "mw-parser-output")
+for el in element_list:
+    print(el.get_attribute("innerText"))
 
 time.sleep(1)
 browser.quit()
