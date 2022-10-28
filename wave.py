56b56c904ff818dd3371874872030867d34979b8
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-10-28 17:44:17 +0900

    周波数っぽい何かにした

--- wave.py
+++ wave.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile  # get the api
+import numpy as np
+
+# load the data
 fs, data = wavfile.read(
-    '/media/sonoda/1AAA3130AA3109B1/Users/ks687/Music/flac/The_Beatles-1/01.Love_Me_Do.wav')  # load the data
+    '/media/sonoda/1AAA3130AA3109B1/Users/ks687/Music/flac/The_Beatles-1/01.Love_Me_Do.wav')
 print(fs, data.shape)
 a = data.T[0]  # this is a two channel soundtrack, I get the first track
 
 # this is 8-bit track, b is now normalized on [-1,1)
 b = [(ele/2**8.)*2-1 for ele in a]
 
-c = fft(b)  # calculate fourier transform (complex numbers list)
+# calculate fourier transform (complex numbers list)
+c = fft(b)
 print(c)
 
-d = len(c)//2  # you only need half of the fft list (real signal symmetry)
+# you only need half of the fft list (real signal symmetry)
+d = len(c) // 2
 
-plt.plot(abs(c[:(d-1)]), 'r')
+fq = np.linspace(0, fs / 2.0, d - 1)
+
+plt.plot(fq, abs(c[:(d-1)]))
 plt.yscale("log")
 plt.show()
