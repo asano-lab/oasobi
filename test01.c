b36bbaa152a922be68b277b24644decfdf748b1d
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-10-31 17:45:08 +0900

    解像度を考えてみた

--- test01.c
+++ test01.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 
 int main(void) {
-    u_short x;
+    u_short x, y;
     x = 0xffff;
-    printf("%d\n", x);
+    y = 24000;
+    printf("x=%d, y=%d\n", x, y);
+    printf("x/y=%f\n", (double)x / (double)y);
+    double r;
+    r = 0.0001;
+    x *= r;
+    y *= r;
+    printf("x=%d, y=%d\n", x, y);
+    printf("x/y=%f\n", (double)x / (double)y);
     return 0;
 }
