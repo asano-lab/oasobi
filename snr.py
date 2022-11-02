7f80410111ec8fe6bdd4afd7b4964e5fd582efe0
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-02 21:10:12 +0900

    変更

--- snr.py
+++ snr.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-BIT_LEN = 32
+BIT_LEN = 3
 MAX_BIT = (1 << BIT_LEN) - 1
+
+GOSA_MAX = 1 / MAX_BIT / 2
 print(MAX_BIT)
+print(GOSA_MAX)
 
 x = np.linspace(0, 1, 101)
 print(x)
