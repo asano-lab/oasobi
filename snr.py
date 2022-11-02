5228029fbd52cb37d392a5d2f158220a89823120
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-02 22:15:16 +0900

    変更　

--- snr.py
+++ snr.py
@@ -1,24 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-BIT_LEN = 16
+BIT_LEN = 32
 MAX_BIT = (1 << BIT_LEN) - 1
 
 MAX_GOSA = 1 / MAX_BIT / 2
 
-x = np.logspace(1, np.log10(MAX_BIT), 1000)
+x = np.logspace(1, np.log10(MAX_BIT), 1000) / MAX_BIT
 # print(x)
 
 # print(x.astype("u8"))
-y = x / MAX_GOSA
+y = x / (MAX_GOSA + x * 1e-5)
 # print(y)
 y = 20 * np.log10(y)
+
+x = 20 * np.log10(x)
 
 fig = plt.figure(figsize=(8, 5))
 ax = fig.add_subplot(111)
 
-ax.set_xscale("log")
+# ax.set_xscale("log")
 # ax.set_yscale("log")
 
 ax.plot(x, y)
+ax.grid()
 plt.show()
