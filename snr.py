d4901e2a68f30e0d4c2b376d7a107a43b1dd1990
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-02 21:20:47 +0900

    謎のグラフができた

--- snr.py
+++ snr.py
@@ -4,16 +4,21 @@ import matplotlib.pyplot as plt
 BIT_LEN = 16
 MAX_BIT = (1 << BIT_LEN) - 1
 
+MAX_GOSA = 1 / MAX_BIT / 2
+
 x = np.logspace(1, np.log10(MAX_BIT), 1000)
 # print(x)
 
 # print(x.astype("u8"))
-y = abs(x - (x + 0.5).astype("u8"))
-print(y)
+y = x / MAX_GOSA
+# print(y)
+y = 20 * np.log10(y)
 
 fig = plt.figure(figsize=(8, 5))
 ax = fig.add_subplot(111)
 
 ax.set_xscale("log")
+# ax.set_yscale("log")
+
 ax.plot(x, y)
 plt.show()
