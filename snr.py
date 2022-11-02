d31e32e25df14ac04d15e5d1f53ac3647dcc7a55
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-02 21:17:33 +0900

    よくわからないグラフができた

--- snr.py
+++ snr.py
@@ -4,9 +4,16 @@ import matplotlib.pyplot as plt
 BIT_LEN = 16
 MAX_BIT = (1 << BIT_LEN) - 1
 
-x = np.logspace(1, np.log10(MAX_BIT), 100)
-print(x)
+x = np.logspace(1, np.log10(MAX_BIT), 1000)
+# print(x)
 
-print(x.astype("u8"))
-y = x - (x + 0.5).astype("u8")
+# print(x.astype("u8"))
+y = abs(x - (x + 0.5).astype("u8"))
 print(y)
+
+fig = plt.figure(figsize=(8, 5))
+ax = fig.add_subplot(111)
+
+ax.set_xscale("log")
+ax.plot(x, y)
+plt.show()
