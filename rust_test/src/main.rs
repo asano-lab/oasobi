1472a89c97ef72ae498204a0d2d7c8810b7c672c
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-09 17:35:07 +0900

    バッファにメッセージが溜まっているらしい

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -1,18 +1,28 @@
 use std::sync::mpsc;
 use std::thread;
-use std::time;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-        // valは{}
-        // println!("val is {}", val);
-    });
-    // thread::sleep(time::Duration::from_millis(1));
+        // スレッドからやあ(hi from the thread)
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
 
-    let received = rx.try_recv().unwrap();
-    println!("Got: {}", received);
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    thread::sleep(Duration::from_millis(1000));
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
