cc8b62ff21237da904743f189dfd642dc9f033e6
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-09 17:41:51 +0900

    senderをクローンすれば複数個同じものが走る?

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -3,21 +3,43 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    // --snip--
+
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
-        // スレッドからやあ(hi from the thread)
-        let vals = vec![(0, 1), (2, 3)];
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
 
         for val in vals {
-            tx.send(val).unwrap();
-            // thread::sleep(Duration::from_millis(1000));
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::spawn(move || {
+        // 君のためにもっとメッセージを(more messages for you)
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 
     for received in rx {
-        println!("Got: {:?}", received);
+        println!("Got: {}", received);
     }
+
+    // --snip--
 }
