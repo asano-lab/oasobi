fd7ea7326a04d37a4213fb89ad21c10d2ac42a7b
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-10 16:10:32 +0900

    急によくわからなくなった

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -13,9 +13,34 @@ macro_rules! bind {
         let $var = $val;
     };
 }
+macro_rules! exact_one {
+    () => {
+        Some(1)
+    };
+}
+
+macro_rules! two {
+    ($x: ident) => (Some($x @ 2))
+}
+
+macro_rules! three {
+    ($x: ident) => (Some($x @ 3))
+}
+
+macro_rules! many_or_none {
+    () => {
+        _
+    };
+}
 
 fn main() {
     call_by_double!(print2, 3); // -> 1, 1
     bind!(x, 3729);
     println!("{}", x);
+    println!("{:?}", exact_one!());
+    match Some(2) {
+        exact_one!() => println!("exact one"),
+        two!(x) | three!(x) => println!("{}", x),
+        many_or_none!() => (),
+    }
 }
