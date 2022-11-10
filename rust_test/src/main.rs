fa06f28b3bbca686ad4c08397e665e92c11ecad7
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2022-11-10 16:02:31 +0900

    マクロの勉強

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -1,27 +1,10 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+macro_rules! add {
+    ($e1: expr, $e2: expr) => {
+        $e1 + $e2
+    };
+}
 
 fn main() {
-    // --snip--
-
-    let (tx, rx) = mpsc::channel();
-
-    for i in 0..3 {
-        let txi = mpsc::Sender::clone(&tx);
-        let i_cp = i.clone();
-        thread::spawn(move || {
-            for j in 0..4 {
-                txi.send(j + i_cp * 100).unwrap();
-                thread::sleep(Duration::from_secs(1));
-            }
-        });
-    }
-    drop(tx);
-
-    for received in rx {
-        println!("Got: {}", received);
-    }
-
-    // --snip--
+    let ret = add!(1, 2);
+    println!("{}", ret); // -> 3
 }
