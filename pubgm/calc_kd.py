cac4efdbac88da62d3c95f064ba596a39e083302
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2023-01-18 21:07:16 +0900

    95%信頼区間らしきものが得られた

--- pubgm/calc_kd.py
+++ pubgm/calc_kd.py
@@ -9,8 +9,11 @@ def main():
     # print(df)
     kill_mean = df["kill"].mean()
     kill_var = df["kill"].var()
-    print(kill_var)
-    print(stats.tvar(df["kill"]))
+    n = df.shape[0]
+    # print(kill_var)
+    # print(stats.tvar(df["kill"]))
+    print(stats.norm.interval(confidence=0.95,
+          loc=kill_mean, scale=np.sqrt(kill_var / n)))
 
 
 if __name__ == "__main__":
