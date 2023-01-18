9f6b3fed18193c20a6042d5d6aa6b715bd4f8e7f
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2023-01-18 21:00:34 +0900

    dfの.varとstats.tvarは同じ?

--- pubgm/calc_kd.py
+++ pubgm/calc_kd.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 def main():
     df = pd.read_csv("kill.csv")
-    print(df)
+    # print(df)
+    kill_mean = df["kill"].mean()
+    kill_var = df["kill"].var()
+    print(kill_var)
+    print(stats.tvar(df["kill"]))
 
 
 if __name__ == "__main__":
