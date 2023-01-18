0e7a66ab60ab5b54f20fb52e1b2eb689b2d427a8
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2023-01-18 21:10:46 +0900

    キル数とダメージの95%信頼区間を計算

--- pubgm/calc_kd.py
+++ pubgm/calc_kd.py
@@ -4,16 +4,21 @@ import numpy as np
 from scipy import stats
 
 
+def calc_interval(ds: pd.Series, alpha=0.95):
+    """
+    平均値の信頼区間を求める
+    """
+    sample_mean = ds.mean()
+    sample_var = ds.var()
+    n = ds.shape[0]
+    return stats.norm.interval(confidence=alpha,
+                               loc=sample_mean, scale=np.sqrt(sample_var / n))
+
+
 def main():
     df = pd.read_csv("kill.csv")
-    # print(df)
-    kill_mean = df["kill"].mean()
-    kill_var = df["kill"].var()
-    n = df.shape[0]
-    # print(kill_var)
-    # print(stats.tvar(df["kill"]))
-    print(stats.norm.interval(confidence=0.95,
-          loc=kill_mean, scale=np.sqrt(kill_var / n)))
+    print(calc_interval(df["kill"]))
+    print(calc_interval(df["damage"]))
 
 
 if __name__ == "__main__":
