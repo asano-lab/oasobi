6f522f6aa3b426e494d44727ed7e8df2d5ec4ccd
Keiichiro Sonoda <21w2039g@shinshu-u.ac.jp>
2023-03-02 06:13:25 +0900

    ちゃんと正規分布らしい

--- random_normal.py
+++ random_normal.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-a = np.random.normal(0.0, 1.0, 100)
+a = np.random.normal(0.0, 1.0, 1000)
 print(a)
+
+fig = plt.figure(figsize=(8, 5))
+ax = fig.add_subplot(111)
+ax.hist(a)
+
+plt.show()
